c1a888e028fe27d09a9d5c15575a8500dcf1af05
Sami Wagiaalla <swagiaal@redhat.com>
2005-08-11 18:14:05 +0000

    Created Makefile.am configure.ac to build procpop using atuomake Added EggTrayIcon code and rules to build the .so

---
+++ frysk-gui/EggTrayIcon/libEggTrayIcon.c
@@ -0,0 +1,42 @@
+
+#ifndef __com_redhat_fedora_frysk_gui_EggTrayIcon__
+#define __com_redhat_fedora_frysk_gui_EggTrayIcon__
+
+#include <jni.h>
+#include <jg_jnu.h>
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+JNIEXPORT jint JNICALL Java_com_redhat_fedora_frysk_gui_EggTrayIcon_egg_1tray_1icon_1get_1type (JNIEnv *env, jclass c){
+  return (jint)egg_tray_icon_get_type();
+}
+
+JNIEXPORT jobject JNICALL Java_com_redhat_fedora_frysk_gui_EggTrayIcon_egg_1tray_1icon_1new_1for_1screen (JNIEnv *env, jclass c, jobject screen, jstring name){
+  return (jobject)getHandleFromPointer(env,egg_tray_icon_new_for_screen(screen,name));
+}
+
+JNIEXPORT jobject JNICALL Java_com_redhat_fedora_frysk_gui_EggTrayIcon_egg_1tray_1icon_1new (JNIEnv *env, jclass c, jstring name){
+  return (jobject)getHandleFromPointer(env,egg_tray_icon_new(name));
+}
+
+JNIEXPORT jint JNICALL Java_com_redhat_fedora_frysk_gui_EggTrayIcon_egg_1tray_1icon_1send_1message (JNIEnv *env, jclass c, jobject icon, jint timeout, jstring message, jint len){
+  return (jint)egg_tray_icon_send_message(icon,timeout,message,len);
+}
+
+JNIEXPORT void JNICALL Java_com_redhat_fedora_frysk_gui_EggTrayIcon_egg_1tray_1icon_1cancel_1message (JNIEnv *env, jclass c, jobject icon, jint id){
+  egg_tray_icon_cancel_message (icon,id);
+  
+}
+
+JNIEXPORT jint JNICALL Java_com_redhat_fedora_frysk_gui_EggTrayIcon_egg_1tray_1icon_1get_1orientation (JNIEnv *env, jclass c, jobject icon){
+  return (jint)egg_tray_icon_get_orientation (icon);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __com_redhat_fedora_frysk_gui_EggTrayIcon__ */
---
+++ frysk-gui/autogen.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -eu
+# This file is part of FRYSK.
+#
+# Copyright 2005, Red Hat Inc.
+#
+# FRYSK is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# FRYSK is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FRYSK; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# Generate everything (always run with --add-missing).
+
+sh -eu ./common/Makefile.gen.sh com
+
+echo "Running aclocal ..."
+aclocal
+
+echo "Running autoconf ..."
+autoconf
+
+echo "Running automake ..."
+automake --add-missing
