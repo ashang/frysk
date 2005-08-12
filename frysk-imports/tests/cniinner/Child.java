9d93a90ef6635fc9c9aa06d49b87fda2546d3635
Andrew Cagney <cagney@redhat.com>
2005-08-12 16:22:34 +0000

    2005-08-12  Andrew Cagney  <cagney@redhat.com>
    	* Makefile.am, configure.ac: Add directory tests/cniinner.
    	* tests/cniinner/.cvsignore, tests/cniinner/Child.java,
    	tests/cniinner/Makefile.am, tests/cniinner/Parent.java,
    	tests/cniinner/native.cxx: New.

---
+++ frysk-imports/tests/cniinner/Child.java
@@ -0,0 +1,21 @@
+package cniinner;
+
+class Child
+{
+    int i;
+    class Nested extends Parent
+    {
+	boolean b;
+	native void f ();
+    }
+    Nested nested ()
+    {
+      return new Nested ();
+    }
+    static public void main (String[] args)
+    {
+	Child c = new Child ();
+	Nested n = c.nested ();
+	n.f ();
+    }
+}
