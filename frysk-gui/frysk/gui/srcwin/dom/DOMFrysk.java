75c0c7f8797050717feb8f90dbe654528bbc3048
Rick Moseley <rmoseley@redhat.com>
2005-10-03 15:08:34 +0000

    * dom/DOMFrysk.java: added addImage() method 						 added checkImageDup() method 						 modified getImage() method
    	* dom/DOMTestDOMFrysk.java: added tests for above methods plus some others

--- frysk-gui/frysk/gui/srcwin/dom/DOMFrysk.java
+++ frysk-gui/frysk/gui/srcwin/dom/DOMFrysk.java
@@ -21,10 +21,17 @@ public class DOMFrysk {
 	/**
 	 * The pid of the process this DOM represents
 	 */
-	public static final String PID_ATTR = "pid";
+	private static final String PID_ATTR = "pid";
+
 	private static final String PC_ATTR = "PC";
-	public static final String pcValue = "value";
+
+	private static final String pcValue = "value";
+
+	private static final String CCPATH_ATTR = "CCPATH";
+
 	private static final Element pcName = new Element(PC_ATTR);
+
+	private static final String IMAGE_ATTR = "image";
 
 	private Document data;
 
@@ -39,7 +46,50 @@ public class DOMFrysk {
 		this.data.getRootElement().setText("Frysk JDOM");
 		this.data.getRootElement().addContent(pcName);
 	}
+
+	/**
+	 * adds an image element to the DOM
+	 * 
+	 * @param image_name =
+	 *            the name of the image to be added
+	 * @param CCPATH =
+	 *            the CCPATH associated with this image
+	 * @param source_path =
+	 *            the path to the source of this image
+	 * @return
+	 */
+	public boolean addImage(String image_name, String CCPATH, String source_path) {
+		// Make sure this image name is not already there before adding
 	
+		if (checkImageDup(image_name))
+			return false;
+		Element imageNameElement = new Element(IMAGE_ATTR);
+		imageNameElement.setAttribute(DOMImage.NAME_ATTR, image_name);
+		imageNameElement.setAttribute(CCPATH_ATTR, CCPATH);
+		this.data.getRootElement().addContent(imageNameElement);
+		return true;
+	}
+
+	/**
+	 * checkImageDup - check to see if there is a duplicate image name
+	 * 
+	 * @param String image_name =
+	 *            name of the image to check for
+	 */
+	
+	public boolean checkImageDup(String image) {
+		Iterator i = this.data.getRootElement().getChildren().iterator();
+		while (i.hasNext()) {
+			Element elem = (Element) i.next(); 
+			if (elem.getQualifiedName().equals(IMAGE_ATTR)) {
+				if (elem.getAttributeValue(DOMImage.NAME_ATTR)
+						.equals(image))
+					return true;
+			}
+		}
+		return false;
+	} 
+
 	/**
 	 * Retrieves all the images contained in the DOM as an iterator
 	 * 
@@ -71,8 +121,11 @@ public class DOMFrysk {
 
 		while (i.hasNext()) {
 			Element elem = (Element) i.next();
-			if (elem.getAttributeValue(DOMImage.NAME_ATTR).equals(name))
-				return new DOMImage(elem);
+			if (elem.getQualifiedName().equals(IMAGE_ATTR)) {
+				if (elem.getAttributeValue(DOMImage.NAME_ATTR)
+						.equals(name))
+					return new DOMImage(elem);
+			}
 		}
 
 		return null;
@@ -92,18 +145,17 @@ public class DOMFrysk {
 	protected Element getElement() {
 		return this.data.getRootElement();
 	}
-	
+
 	/**
 	 * 
 	 * @return BigInteger program counter
 	 */
 	public BigInteger getPC() {
-		BigInteger bInt = 
-			new BigInteger(this.data.getRootElement().getChild(PC_ATTR
-					).getAttribute(pcValue).getValue());
-		return bInt;	
+		BigInteger bInt = new BigInteger(this.data.getRootElement().getChild(
+				PC_ATTR).getAttribute(pcValue).getValue());
+		return bInt;
 	}
-	
+
 	/**
 	 * Set the PC counter value in the DOM
 	 */
@@ -125,6 +177,13 @@ public class DOMFrysk {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	/**
+	 * Return the DOMFrysk document
+	 */
+	public Document getDOMFrysk() {
+		return this.data;
 	}
 
 }
--- frysk-gui/frysk/gui/srcwin/dom/DOMTestDOMFrysk.java
+++ frysk-gui/frysk/gui/srcwin/dom/DOMTestDOMFrysk.java
@@ -11,16 +11,45 @@ import org.jdom.Element;
 
 public class DOMTestDOMFrysk {
 	private static BigInteger pc;
-	
+
 	public static void main(String[] args) {
-	
-	Element root = new Element("Frysk_test");
-	Document data = new Document(root);
-	DOMFrysk dom = new DOMFrysk(data);
-	pc = new BigInteger("25");
-	dom.setPC(pc);
-	System.out.println("PC = " + dom.getPC());
-	dom.printDOM();
-	
+
+		Element root = new Element("Frysk_test");
+		Document data = new Document(root);
+		DOMFrysk dom = new DOMFrysk(data);
+		pc = new BigInteger("25");
+		dom.setPC(pc);
+		if (dom.addImage("test_image", "/home/xyz", "/usr/src/redhat")) {
+			System.out.println("Adding first image(test_image) passed.");
+		} else {
+			System.out.println("Adding first image(test_image) failed.");
+		}
+		if (dom.addImage("test_image_2", "/usr/src/redhat", "/tmp")) {
+			System.out.println("Adding first image(test_image_2) passed.");
+		} else {
+			System.out.println("Adding first image(test_image_2) failed.");
+		}
+		if (!dom.addImage("test_image", "nada", "nada")) {
+			System.out.println("Tried to add duplicate image test passed.");
+		} else {
+			System.out.println("Duplicate imaqe add test failed.");
+		}
+		if (dom.getPC().equals(pc)) {
+			System.out.println("Get PC test passed. pc = " + dom.getPC());
+		} else {
+			System.out.println("Get PC test failed.");
+		}
+		if (dom.getImage("test_image_2").getName().equals("test_image_2.")) {
+			System.out.println("DOMGetImage test passed for test_image_2.");
+		} else {
+			System.out.println("DOMGetImage test failed for test_image_2.");
+		}
+//		if (dom.getImage("abc_xyz").equals(null)) {
+//			System.out.println("DOMGetImage test passed for abc_xyz");
+//		} else {
+//			System.out.println("DOMGetImage test failed for abc_xyz");
+//		}
+		System.out.println("\n\n");
+		dom.printDOM();
 	}
 }
