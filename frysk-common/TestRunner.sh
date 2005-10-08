e25b9dfedad1e3e435636e4f079c54a7c30a60bb
Andrew Cagney <cagney@redhat.com>
2005-10-08 21:58:06 +0000

    Index: ChangeLog 2005-10-08  Andrew Cagney  <cagney@redhat.com>
    	* TestRunner.sh: Add class Results, extends ResultPrinter.  Use
    	Results to print each failure as soon as it occures.

--- frysk-common/TestRunner.sh
+++ frysk-common/TestRunner.sh
@@ -3,10 +3,55 @@
 cat <<EOF
 import junit.framework.TestSuite;
 import junit.framework.TestResult;
+import junit.framework.AssertionFailedError;
+import junit.textui.ResultPrinter;
+import junit.framework.Test;
+import java.io.PrintStream;
 
 public class TestRunner
     extends junit.textui.TestRunner
 {
+    TestRunner (ResultPrinter resultPrinter)
+    {
+	super (resultPrinter);
+    }
+
+
+    static class Results
+	extends ResultPrinter
+    {
+	public void startTest (Test test)
+	{
+	    System.out.print ("Running ");
+	    System.out.print (test);
+	    System.out.print (" ...");
+	    System.out.flush ();
+	}
+	public void endTest (Test test)
+	{
+	    System.out.println ();
+	}
+	public void addError (Test test, java.lang.Throwable t)
+	{
+	    System.out.print (" ERROR: ");
+	    t.printStackTrace ();
+	}
+	public void addFailure (Test test, AssertionFailedError t)
+	{
+	    System.out.print (" FAIL: ");
+	    t.printStackTrace ();
+	}
+	protected void printDefects(java.util.Enumeration booBoos,
+				    int count, java.lang.String type)
+	{
+	    // Do nothing.
+	}
+	Results (PrintStream stream)
+	{
+	    super (stream);
+	}
+    }
+
     static Class[] testClasses = new Class[] {
 EOF
 
@@ -24,10 +69,10 @@ cat <<EOF
 	};
     public static void main (String[] args)
     {
+	TestRunner runner = new TestRunner (new Results (System.out));
         TestSuite testSuite = new TestSuite ();
         if (args.length > 0) {
 	    // Construct the testsuite from the list of names.
-            TestRunner runner = new TestRunner ();
             for (int i = 0; i < args.length; i++) {
                 testSuite.addTest (runner.getTest (args[i]));
             }
@@ -41,7 +86,7 @@ EOF
 
 cat <<EOF
         try {
-	    TestResult testResult = run (testSuite);
+	    TestResult testResult = runner.doRun (testSuite);
 	    if (!testResult.wasSuccessful()) 
 		System.exit (FAILURE_EXIT);
 	    System.exit(SUCCESS_EXIT);
