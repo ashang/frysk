321ae0a9c5a522ce4dd22c795d1cbda6f3a7e82d
Sami Wagiaalla <swagiaal@redhat.com>
2005-10-13 13:37:27 +0000

    * observers/SyscallObserver.java: new file.

---
+++ frysk-gui/frysk/gui/monitor/observers/SyscallObserver.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Oct 11, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package frysk.gui.monitor.observers;
+
+public class SyscallObserver extends ObserverRoot {
+
+	public SyscallObserver() {
+		super("Syscall Observer", "Fires when a system call is made.");
+	}
+}
