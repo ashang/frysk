3a0ddcdc27b46d38b61db76015b781302ea764df
Adam Jocksch <ajocksch@redhat.com>
2005-10-20 15:06:58 +0000

    2001-10-20  Adam Jocksch  <ajocksch@redhat.com>
    	* Makefile.am, configure.ac: Added tests/textiter, tests/gcjtreeiter,
    	removed tests/gcc_4_0_15
    	* tests/gcc_4_0_15: contents moved to tests/textiter.
    	* tests/gcjtreeiter: Added.
    	* tests/gcjtreeiter/Makefile.am: Added.
    	* tests/gcjtreeiter/IterTest.java: Added.

---
+++ frysk-imports/tests/gcjtreeiter/IterTest.java
@@ -0,0 +1,31 @@
+package gcjtreeiter;
+import org.gnu.gtk.DataColumn;
+import org.gnu.gtk.DataColumnString;
+import org.gnu.gtk.Gtk;
+import org.gnu.gtk.ListStore;
+import org.gnu.gtk.TreeIter;
+
+public class IterTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Gtk.init(args);
+		
+		DataColumn[] cols = new DataColumn[] {new DataColumnString()};
+		
+		ListStore store = new ListStore(cols);
+		
+		for(int i = 0; i < 10; i++){
+			TreeIter iter = store.appendRow();
+			store.setValue(iter, (DataColumnString) cols[0], ""+i);
+		}
+		
+		TreeIter iter = store.getFirstIter();
+		
+		while(iter != null)
+			iter = iter.getNextIter();
+	}
+
+}
---
+++ frysk-imports/tests/textiter/JNICrash.java
@@ -0,0 +1,23 @@
+package textiter;
+import org.gnu.gtk.Gtk;
+import org.gnu.gtk.TextBuffer;
+
+public class JNICrash {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Gtk.init(args);
+		
+		TextBuffer b = new TextBuffer();
+		b.insertText("This is a test\nThat spans multiple lines");
+		b.createMark("TestMark", b.getLineIter(1), true);
+		b.deleteText(b.getStartIter(), b.getEndIter());
+		b.insertText("This is another test\nThat will hopefully break");
+		b.createMark("TestMark", b.getLineIter(1), true);
+		
+		// We don't want to enter the main loop, either it will die by this point or it won't
+	}
+
+}
