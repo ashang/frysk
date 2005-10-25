f3d25ae4d2345e00ed881c4bc21c758f6bbf203e
Rick Moseley <rmoseley@redhat.com>
2005-10-25 11:56:31 +0000

    * testfiles/test6.cpp: new for testing DOM/GUI

---
+++ frysk-gui/frysk/gui/srcwin/testfiles/test6.cpp
@@ -0,0 +1,14 @@
+// this is a test program for the DOM
+class bar
+{
+public:
+  inline int min(int y, int z);
+};
+// main program
+int main()
+{
+  bar a;
+  int i = min(1,2);
+  int j = min(3,4);
+  return 0;
+}
