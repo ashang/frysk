5d46731a3e116e0ae0261a9703630b68b1ba8277
Chris Moller <cmoller@redhat.com>
2005-11-08 20:02:31 +0000

    Changed package to com.redhat.ftk

--- frysk-imports/tlwidgets/src/java/org/redhat/ftk/Stripchart.java
+++ frysk-imports/tlwidgets/src/java/org/redhat/ftk/Stripchart.java
@@ -7,11 +7,13 @@
  * the terms of the GNU Library General Public License version 2.
  */
 
+//package org.gnu.gtk;
 package com.redhat.ftk;
 
 import org.gnu.glib.GObject;
 import org.gnu.glib.Type;
 import org.gnu.glib.Handle;
+import org.gnu.gtk.Widget;
 
 /**
  * The Stripchart widget is used for creating custom user interface elements.
@@ -19,16 +21,21 @@ import org.gnu.glib.Handle;
  */
 public class Stripchart extends Widget 
 {
-	public Stripchart() {
-		super(ftk_stripchart_new());
-	}
+
+    static {
+	System.loadLibrary ("ftk");
+    }
+
+    public Stripchart() {
+	super(ftk_stripchart_new());
+    }
 	
-	/**
-	 * Construct a Stripchart using a handle to a native resource.
-	 */
-	public Stripchart(Handle handle) {
-	    super(handle);
-	}
+    /**
+     * Construct a Stripchart using a handle to a native resource.
+     */
+    public Stripchart(Handle handle) {
+	super(handle);
+    }
     
     /**
      * Internal static factory method to be used by Java-Gnome only.
@@ -47,16 +54,77 @@ public class Stripchart extends Widget
         return obj;
     }
 
-	/**
-	 * Retrieve the runtime type used by the GLib library.
-	 */
-	public static Type getType() {
-		return new Type(ftk_stripchart_get_type());
-	}
+    /**
+     * Retrieve the runtime type used by the GLib library.
+     */
+    public static Type getType() {
+	return new Type(ftk_stripchart_get_type());
+    }
+
+    /**
+     * Set stripchart size
+     */
+    public void resize(int width, int height) {
+	ftk_stripchart_resize (getHandle(), width, height);
+    }
+
+    /**
+     * Set an event color
+     */
+    public void setEventRGB(int type, int red, int green, int blue) {
+	ftk_stripchart_set_event_rgb (getHandle(), type,
+				      red, green, blue);
+    }
+
+    /**
+     * Set an event title
+     */
+    public void setEventTitle(int type, String title) {
+	ftk_stripchart_set_event_title (getHandle(), type, title);
+    }
+
+    /**
+     * Set stripchart update interval
+     */
+    public void setUpdate(int update) {
+	ftk_stripchart_set_update (getHandle(), update);
+    }
+
+    /**
+     * Set stripchart range interval
+     */
+    public void setRange(int range) {
+	ftk_stripchart_set_range (getHandle(), range);
+    }
+
+    /**
+     * Set stripchart append event
+     */
+    public void appendEvent(int type) {
+	ftk_stripchart_append_event (getHandle(), type);
+    }
 
 
-    native static final protected int ftk_stripchart_get_type ();
-    native static final protected Handle ftk_stripchart_new ();
+    native static final protected int
+	ftk_stripchart_get_type ();
+    native static final protected Handle
+	ftk_stripchart_new ();
+    native static final protected void
+	ftk_stripchart_set_event_rgb (Handle sc,
+				      int type,
+				      int red, int green, int blue);
+    native static final protected void
+	ftk_stripchart_set_event_title(Handle sc,
+				       int type,
+				       String title);
+    native static final protected void
+	ftk_stripchart_resize (Handle sc, int width, int height);
+    native static final protected void
+	ftk_stripchart_set_update (Handle sc, int update);
+    native static final protected void
+	ftk_stripchart_set_range (Handle sc, int range);
+    native static final protected void
+	ftk_stripchart_append_event (Handle sc, int type);
 
 }
 
