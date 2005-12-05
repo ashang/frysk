cfdf5b60cf924a24366cfaea61357b63d51572e2
Andrew Cagney <cagney@redhat.com>
2005-12-05 00:02:07 +0000

    Index: frysk-core/frysk/proc/ChangeLog 2005-12-04  Andrew Cagney  <cagney@redhat.com>
    	* TestGoAndGo.java: Use Host.observableTaskAdded.

--- frysk-core/frysk/proc/TestGoAndGo.java
+++ frysk-core/frysk/proc/TestGoAndGo.java
@@ -65,35 +65,22 @@ public class TestGoAndGo
 	int stopTimerEventCount;
 	int goTimerEventCount;
 	
-	// As soon as the process is created, attach a task created
-	// observer.
-	
-	class ProcCreatedObserver
-	    implements Observer
-	{
-	    int pid;
-	    ProcCreatedObserver (int pid)
-	    {
-		this.pid = pid;
-	    }
-	    public void update (Observable o, Object obj)
-	    {
-		Proc proc = (Proc) obj;
-		if (proc.id.hashCode () != pid)
-		    return;
-		proc.observableTaskAdded.addObserver (new TaskCreatedObserver ());
-	    }
-	}
-	
 	// Once the task has been created, schedule a terminate signal.
 	
 	class TaskCreatedObserver
 	    implements Observer
 	{
+	    int pid;
+	    TaskCreatedObserver (int pid)
+	    {
+		this.pid = pid;
+	    }
 	    int eventCount;
 	    public void update (Observable o, Object obj)
 	    {
 		Task task = (Task) obj;
+		if (task.proc.getPid () != pid)
+		    return;
 		assertEquals ("No termination events before task creation", 0,
 			      taskDestroyedCount);
 		taskCreatedCount++;
@@ -191,7 +178,7 @@ public class TestGoAndGo
 
 	TestGoAndGoInternals (int pid)
 	{
-	    Manager.host.observableProcAdded.addObserver (new ProcCreatedObserver (pid));
+	    Manager.host.observableTaskAdded.addObserver (new TaskCreatedObserver (pid));
 	    new TaskDestroyedObserver ();
 	}
     }
