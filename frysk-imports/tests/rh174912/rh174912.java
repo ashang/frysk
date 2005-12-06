73c69e9ce9b1236283e68dbaf98a3efefa42d7d1
Andrew Cagney <cagney@redhat.com>
2005-12-06 20:54:45 +0000

    2005-12-06  Andrew Cagney  <cagney@redhat.com>
            * Makefile.am (rh174912/gcj.sh): Steal from tom tromey.

---
+++ frysk-imports/tests/rh174912/rh174912.java
@@ -0,0 +1,12 @@
+public class rh174912 {
+  public rh174912(char[][] args) { }
+
+  public Object m() {
+    return new rh174912(new char[][] { "hi".toCharArray(),
+				       "bob".toCharArray(),
+				       "and joe".toCharArray() }) {
+      };
+  }
+
+  public static void main(String[] args) { }
+}
