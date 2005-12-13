e2dbae5ff674704166fb83be5b9fb301a0dc2ab2
Andrew Cagney <cagney@redhat.com>
2005-12-13 03:50:20 +0000

    Index: frysk-imports/tests/ChangeLog 2005-12-12  Andrew Cagney  <cagney@redhat.com>
    	* Makefile.am (rh175569/gcj.sh): Add target.
    	* rh175569/C.java: New.

---
+++ frysk-imports/tests/rh175569/C.java
@@ -0,0 +1,11 @@
+class C
+{
+    private static final int CONST  = 0;
+    class I {
+        public void f () {
+            switch(0) {
+            case CONST:
+            }
+        }
+    }
+}
