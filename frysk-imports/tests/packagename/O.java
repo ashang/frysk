508a10fad57b940fcece3c6c3e7167e3bb4acdbc
Andrew Cagney <cagney@redhat.com>
2006-01-03 21:08:20 +0000

    Index: ChangeLog 2006-01-03  Andrew Cagney  <cagney@redhat.com>
    	* packagename/O.java: New file.  From Tom Tromey.
    	* Makefile.am (packagename/a.out): New target.

---
+++ frysk-imports/tests/packagename/O.java
@@ -0,0 +1,8 @@
+package packagename;
+public class O {
+  public static void main(String[] args) throws Throwable {
+    Package p = O.class.getPackage();
+    System.out.println(p);
+    if (p == null) throw new RuntimeException ("no package name");
+  }
+}
