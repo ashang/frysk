42e76e17f360cae24b17735c5a1e7da553bbc914
Andrew Cagney <cagney@redhat.com>
2006-01-26 19:50:11 +0000

    Index: frysk-gtk/ChangeLog 2006-01-26  Andrew Cagney  <cagney@redhat.com>
    	* Makefile.am (EggTrayIcon_libEggTrayIcon_a_CFLAGS): Delete.
    	(AM_CFLAGS): Add $(GTK_CFLAGS) and $(FRYSK_GTK_CFLAGS).

--- frysk-gtk/EggTrayIcon/libEggTrayIcon.c
+++ frysk-gtk/EggTrayIcon/libEggTrayIcon.c
@@ -4,22 +4,19 @@
 
 #include <jni.h>
 #include <jg_jnu.h>
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
+#include "eggtrayicon.h"
+#include <gtk_java.h>
 
 JNIEXPORT jint JNICALL Java_org_gnu_gtk_frysk_EggTrayIcon_egg_1tray_1icon_1get_1type (JNIEnv *env, jclass c){
   return (jint)egg_tray_icon_get_type();
 }
 
 JNIEXPORT jobject JNICALL Java_org_gnu_gtk_frysk_EggTrayIcon_egg_1tray_1icon_1new_1for_1screen (JNIEnv *env, jclass c, jobject screen, jstring name){
-  return getGObjectHandle(env,egg_tray_icon_new_for_screen(screen,name));
+  return getGObjectHandle(env, G_OBJECT (egg_tray_icon_new_for_screen(screen,name)));
 }
 
 JNIEXPORT jobject JNICALL Java_org_gnu_gtk_frysk_EggTrayIcon_egg_1tray_1icon_1new (JNIEnv *env, jclass c, jstring name){
-  return getGObjectHandle(env,egg_tray_icon_new(name));
+  return getGObjectHandle(env,G_OBJECT (egg_tray_icon_new(name)));
 }
 
 JNIEXPORT jint JNICALL Java_org_gnu_gtk_frysk_EggTrayIcon_egg_1tray_1icon_1send_1message (JNIEnv *env, jclass c, jobject icon, jint timeout, jstring message, jint len){
@@ -34,9 +31,5 @@ JNIEXPORT void JNICALL Java_org_gnu_gtk_frysk_EggTrayIcon_egg_1tray_1icon_1cance
 JNIEXPORT jint JNICALL Java_org_gnu_gtk_frysk_EggTrayIcon_egg_1tray_1icon_1get_1orientation (JNIEnv *env, jclass c, jobject icon){
   return (jint)egg_tray_icon_get_orientation (icon);
 }
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* __org_gnu_gtk_frysk_EggTrayIcon__ */
