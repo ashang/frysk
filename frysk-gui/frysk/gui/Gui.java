aa41a0e9a638d272b48fd69277586105ab501324
Phil Muldoon <pmuldoon@redhat.com>
2006-04-06 19:18:31 +0000

    2006-04-06  Phil Muldoon  <pmuldoon@redhat.com>
    	* Gui.java (setupErrorLogging): Set error echo to console
    	back on.

--- frysk-gui/frysk/gui/Gui.java
+++ frysk-gui/frysk/gui/Gui.java
@@ -212,7 +212,8 @@ public class Gui
 	// Get a logger; the logger is automatically created if it
 	// doesn't already exist
 	errorLogFile = Logger.getLogger(ERROR_LOG_ID);
-	errorLogFile.setUseParentHandlers(false);
+	//errorLogFile.setUseParentHandlers(false);
+	errorLogFile.setUseParentHandlers(true);
 	errorLogFile.addHandler(buildHandler());
     }
 
