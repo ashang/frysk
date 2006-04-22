fca55ef245c6e9c900ffca5fa7d9d16d5894151f
Andrew Cagney <cagney@redhat.com>
2006-04-22 17:05:49 +0000

    Index: frysk-imports/tests/ChangeLog 2006-04-22  Andrew Cagney  <cagney@redhat.com>
    	* frysk2595/ptrace_after_exec_of_thread.c: New.
    	* frysk2595/ptrace_after_exec.c: New.
    	* Makefile.am: Add to TESTS.
    	* frysk2595/ptrace_before_forked_thread_exits.c: New.
    	* Makefile.am: Add to XFAIL_TESTS.

---
+++ frysk-imports/tests/frysk2595/ptrace_after_exec.c
@@ -0,0 +1,96 @@
+#define _GNU_SOURCE
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <linux/unistd.h>
+#include <pthread.h>
+
+#define __REENTRANT
+
+_syscall2(int, tkill, int, tid, int, sig);
+
+pid_t c_pid;
+
+int 
+main (int argc, char **argv) 
+{
+  switch (argc)
+    {
+    default:
+      printf ("whats with the %d arguments\n", argc);
+      exit (1);
+
+    case 1: // First time run
+      {
+	c_pid = fork();
+	switch (c_pid)
+	  {
+	  case -1:
+	    {
+	      perror("Error: could not fork child process");
+	      exit(1);
+	    }
+	
+	  case 0:
+	    {
+	      // Child
+	      long ptrace_res = ptrace (PTRACE_TRACEME, 0, NULL, NULL);
+	      if (ptrace_res == -1)
+	      {
+		perror("Error: could not start trace");
+		exit(1);
+	      }
+	      
+	      char* const args[] = {"/bin/true", NULL};
+	      
+	      execv(args[0], args);
+	      
+	      perror("Exec returned. Whoops!");
+	      exit(1);
+	    }
+
+	  default:
+	    {
+	      // Parent
+	      errno = 0;
+	      int status;
+	      pid_t wait_res = waitpid (c_pid, &status, __WALL); 
+	      if (wait_res != c_pid)
+		{
+		  perror("waitpid didn't return the child pid");
+		  exit(1);
+		}
+	      // Now exec ourselves with the pid as a parameter.
+	      char *args[3];
+	      args[0] = argv[0];
+	      asprintf (&args[1], "%d", c_pid);
+	      args[2] = NULL;
+	      printf ("execing %s %s\n", args[0], args[1]);
+	      execv (args[0], args);
+	      perror ("execing with ptraced child\n");
+	      exit (1);
+	    }
+	  }
+      }
+
+    case 2:// The exec of this process
+      {
+	c_pid = atol (argv[1]);
+	printf ("after exec, pid is %d\n", c_pid);
+	errno = 0;
+	ptrace(PTRACE_PEEKUSER, c_pid, 0, 0);
+	if (errno != 0)
+	  {
+	    perror("Ptrace after exec");
+	    exit(1);
+	  }
+      }
+    }
+  tkill(c_pid, SIGKILL);
+  return 0;
+}
