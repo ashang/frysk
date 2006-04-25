7ac9807caf125084ddb5be05f4aa7565cc0ac9a5
Phil Muldoon <pmuldoon@redhat.com>
2006-04-25 21:16:52 +0000

    2006-04-25  Phil Muldoon  <pmuldoon@redhat.com>
            * druid/AddedProcTreeView.java: Removed. Simplify with CheckedListView.
            * druid/ExitProcessGroupsDataModel.java: Ditto.
            * druid/ProcessExitSelectionTreeView.java: Ditto.
    
            * druid/CreateFryskSessionDruid.java: Added new Session instance.
            (addProcessParent): Add routine to add new process types to live session
            data model.
            (changeGroupState): Modify and simply the 3 corner cases for adding a process
            group to a live sesssion object (no parent, parent only, parent and children).
            (getProcessSelectionControls): Mount the addedProcTreeView using Listview. Link
            datamodel to session object. Rework state changes to follow simplified states.
            (getProcessExitControls): Mount the processExitSelectionTreeView using the
            CheckedListView. Link datamodel to session object. Add TaskTerminatingObserver
            on selection of CellRendererToggle.
            (nextPage): Simply, and removing data model swapping. New Session model
            does not need this (model is now unified).

--- frysk-gui/frysk/gui/druid/CreateFryskSessionDruid.java
+++ frysk-gui/frysk/gui/druid/CreateFryskSessionDruid.java
@@ -40,6 +40,8 @@
 package frysk.gui.druid;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.gnu.glade.LibGlade;
 import org.gnu.gtk.Button;
 import org.gnu.gtk.ComboBox;
@@ -53,18 +55,31 @@ import org.gnu.gtk.TreePath;
 import org.gnu.gtk.TreeView;
 import org.gnu.gtk.event.ButtonEvent;
 import org.gnu.gtk.event.ButtonListener;
+import org.gnu.gtk.event.CellRendererToggleEvent;
+import org.gnu.gtk.event.CellRendererToggleListener;
 
+import frysk.gui.monitor.CheckedListView;
+import frysk.gui.monitor.ListView;
+import frysk.gui.monitor.ProcData;
 import frysk.gui.monitor.ProcWiseDataModel;
 import frysk.gui.monitor.ProcWiseTreeView;
+import frysk.gui.monitor.observers.ObserverRoot;
+import frysk.gui.monitor.observers.TaskTerminatingObserver;
+import frysk.gui.sessions.DebugProcess;
+import frysk.gui.sessions.Session;
+import frysk.proc.Proc;
 
 public class CreateFryskSessionDruid extends Dialog {
 	
 	Notebook notebook;
 
 	ProcWiseDataModel dataModel;
-	ExitProcessGroupsDataModel processExitListStore;
+	ProcWiseTreeView procWiseTreeView;
+	ListView addedProcsTreeView;
+	ProcSelectionDataModel selectedModel;
 	ObserverDataModel observerDataModel;
 	ProcessObserverDataModel processObserverDataModel;
+	Session currentSession = new Session();
 	
 	Button nextButton;
 	Button backButton;
@@ -73,9 +88,7 @@ public class CreateFryskSessionDruid extends Dialog {
 	ArrayList processGroupSelection;
 	
 	int processSelected = 0;
-	
 
-	
 	public CreateFryskSessionDruid(LibGlade glade){
 		super(glade.getWidget("SessionDruid").getHandle());		
 		getDruidStructureControls(glade);
@@ -90,7 +103,29 @@ public class CreateFryskSessionDruid extends Dialog {
 		this.dataModel.setSelected(selected,setSelected,setChildren);
 	}
 	
-	
+	private void addProcessParent(TreeIter unfilteredProcessIter) {
+		if (unfilteredProcessIter == null)
+			return;
+		
+		if (!this.dataModel.getModel().isIterValid(unfilteredProcessIter))
+			return;
+			
+		ProcData proc = (ProcData) this.dataModel.getModel().getValue(unfilteredProcessIter,this.dataModel.getPathDC());
+		if (proc != null)
+		{
+			Proc coreProc = proc.getProc();
+			if (coreProc == null)
+				if (unfilteredProcessIter.getChildCount() > 0)
+				{
+					TreeIter childIter = unfilteredProcessIter.getChild(0);
+					coreProc = ((ProcData)this.dataModel.getModel().getValue(childIter,this.dataModel.getPathDC())).getProc();
+				}
+			DebugProcess debugProcess = new DebugProcess(coreProc.getCommand());
+			debugProcess.setProc(coreProc);
+			currentSession.addProcess(debugProcess);
+		}
+	}
+		
 	private boolean isChild(TreePath pathTest)
 	{
 		// Minor hack. Sometimes path.up() returns a boolean
@@ -115,20 +150,27 @@ public class CreateFryskSessionDruid extends Dialog {
 
 	
 		
-	private void changeGroupState(TreeView tree, TreePath[] selectedProcs, boolean state)
+	private void changeGroupState(TreeView tree, TreePath[] selectedProcs, boolean filtered, boolean state)
 	{
 		if (selectedProcs.length > 0)
 		{
 			for(int i=0; i<selectedProcs.length;i++)
 			{
 				// Convert a filetered iterator to a non filtered iterator.
-				TreeIter unfilteredProcessIter = this.dataModel.getModel().getIter(deFilterPath(tree,selectedProcs[i]));
+				TreeIter unfilteredProcessIter;
+				if (filtered)
+					unfilteredProcessIter = this.dataModel.getModel().getIter(deFilterPath(tree,selectedProcs[i]));
+				else
+					unfilteredProcessIter = this.dataModel.getModel().getIter(selectedProcs[i]);
 				
-				// Scenario 1: Tree iter has children (a process group)
+				// Scenario 1: Tree iter has children (a process group); selected the parent
 				if (unfilteredProcessIter.getChildCount() > 0)
 				{
 					if (state)
+					{
 						processSelected += unfilteredProcessIter.getChildCount() +1;
+						addProcessParent(unfilteredProcessIter);
+					}
 					else
 						processSelected -= unfilteredProcessIter.getChildCount() +1;
 					setTreeSelected(unfilteredProcessIter,state,true);
@@ -147,7 +189,10 @@ public class CreateFryskSessionDruid extends Dialog {
 						if (parent_iter.getChildCount() > 0)
 						{
 							if (state)
+							{
 								processSelected += unfilteredProcessIter.getChildCount() +1;
+								addProcessParent(parent_iter);
+							}
 							else
 								processSelected -= unfilteredProcessIter.getChildCount() +1;
 							setTreeSelected(parent_iter,state,true);
@@ -155,22 +200,24 @@ public class CreateFryskSessionDruid extends Dialog {
 					}
 					else
 					{
-						// Scenario 3: No children, or siblings
-						setTreeSelected(unfilteredProcessIter,state,false);
 						if (state)
+						{
+							addProcessParent(unfilteredProcessIter);
 							processSelected++;
+						}
 						else
 							processSelected--;
-						
+						// Scenario 3: No children, or siblings
+						setTreeSelected(unfilteredProcessIter,state,false);
 					}
 				}
-				
 			}
 		}
 		
 		setProcessNext(processSelected);
 	}
 	
+
 	private void setProcessNext(int processCount)
 	{
 		if (processCount > 0)
@@ -184,15 +231,17 @@ public class CreateFryskSessionDruid extends Dialog {
 		Button addProcessGroupButton;
 		Button removeProcessGroupButton;
 		ComboBox hostSelection;
-		
-		final ProcWiseTreeView procWiseTreeView;
-		final AddedProcTreeView addedProcsTreeView;
 
 		// Page 1 of the Druid. Initial Process Selection.
 		
+		// Create New Live Data Model and mount on the TreeView
 		this.dataModel = new ProcWiseDataModel();
 		procWiseTreeView = new ProcWiseTreeView(glade.getWidget("sessionDruid_procWiseTreeView").getHandle(),this.dataModel);
-		addedProcsTreeView = new AddedProcTreeView(glade.getWidget("sessionDruid_addedProcsTreeView").getHandle(),this.dataModel);
+		
+		// Create a New ListView and mount the Linked List from Session data 
+		addedProcsTreeView = new ListView(glade.getWidget("sessionDruid_addedProcsTreeView").getHandle());
+		addedProcsTreeView.watchLinkedList(currentSession.getProcesses());
+		
 		hostSelection = (ComboBox) glade.getWidget("sessionDruid_hostComboBox");
 		this.setUpCurrentPage();
 			
@@ -206,7 +255,8 @@ public class CreateFryskSessionDruid extends Dialog {
 		addProcessGroupButton.addListener(new ButtonListener(){
 			public void buttonEvent(ButtonEvent event) {
 				if(event.isOfType(ButtonEvent.Type.CLICK)){
-					changeGroupState(procWiseTreeView,procWiseTreeView.getSelection().getSelectedRows(),true);
+					changeGroupState(procWiseTreeView,procWiseTreeView.getSelection().getSelectedRows(),true,true);
+					
 				}
 			}
 			
@@ -215,7 +265,14 @@ public class CreateFryskSessionDruid extends Dialog {
 		removeProcessGroupButton.addListener(new ButtonListener(){
 			public void buttonEvent(ButtonEvent event) {
 				if(event.isOfType(ButtonEvent.Type.CLICK)){
-					changeGroupState(addedProcsTreeView,addedProcsTreeView.getSelection().getSelectedRows(),false);
+					Iterator i = addedProcsTreeView.getSelectedObjects();
+					while (i.hasNext())
+					{
+						DebugProcess currentDebugProcess = (DebugProcess) i.next();
+						TreePath foo = dataModel.searchName(currentDebugProcess.getName());
+						changeGroupState(procWiseTreeView,new TreePath[]{foo},false,false);
+						currentSession.removeProcess(currentDebugProcess);
+					}
 				}
 			}
 		});
@@ -226,21 +283,46 @@ public class CreateFryskSessionDruid extends Dialog {
 	
 	private void getProcessExitControls(LibGlade glade)
 	{
-	
-		ProcessExitSelectionTreeView processExitSelectionTreeView;
-		this.processExitListStore = new ExitProcessGroupsDataModel();
-		processExitSelectionTreeView = new ProcessExitSelectionTreeView(
-				glade.getWidget("sessionDruid_unexpectedExitTreeView").getHandle(),this.processExitListStore);	
-		
+		final CheckedListView processExitSelectionTreeView = new CheckedListView(
+				glade.getWidget("sessionDruid_unexpectedExitTreeView").getHandle());
+
+		processExitSelectionTreeView.watchLinkedList(currentSession.getProcesses());
+		processExitSelectionTreeView.getCellRendererToggle().addListener(new CellRendererToggleListener() {
+
+			public void cellRendererToggleEvent(CellRendererToggleEvent arg0) {
+				DebugProcess selected = ((DebugProcess)processExitSelectionTreeView.getSelectedObject());
+				if (processExitSelectionTreeView.isChecked(selected))
+				{
+					System.out.println("Tree has been checked for process: " + selected.getName() +", add task observer if not found");
+					Iterator i = selected.getObservers().iterator();
+					while (i.hasNext())
+						if (((ObserverRoot)i.next()).getName().equals("Task Terminating Observer"))
+							return;
+					
+					System.out.println("Task Term not found, adding");
+					selected.addObserver(new TaskTerminatingObserver());
+					
+				}
+				else
+				{
+					System.out.println("Uncheck event recieved. Removing observer.");
+					selected.removeObserver(new TaskTerminatingObserver());
+				}
+				
+//				Iterator i = selected.getObservers().iterator();
+//				System.out.println("Process " + selected.getName()+" has the following observers: ");
+//				while (i.hasNext())
+//					((ObserverRoot)i.next()).getName();
+					
+			}});
+
 		processExitSelectionTreeView.expandAll();
 		setUpCurrentPage();
 	}
 
 	private void getProcessObserverControls(LibGlade glade) {
 		final ObserverSelectionTreeView observerSelectionTreeView;
-		final ProcessObserverSelectionTreeView processObserverSelectionTreeView;
-
-		
+		final ProcessObserverSelectionTreeView processObserverSelectionTreeView;		
 		
 		this.observerDataModel = new ObserverDataModel();
 		observerSelectionTreeView = new ObserverSelectionTreeView(
@@ -258,9 +340,7 @@ public class CreateFryskSessionDruid extends Dialog {
 		SizeGroup sizeGroup = new SizeGroup(SizeGroupMode.BOTH);
 		sizeGroup.addWidget(observerSelectionTreeView);
 		sizeGroup.addWidget(processObserverSelectionTreeView);
-		
-
-		
+			
 		setUpCurrentPage();	
 	}
 
@@ -289,7 +369,6 @@ public class CreateFryskSessionDruid extends Dialog {
 		});
 		
 		this.finishButton = (Button) glade.getWidget("sessionDruid_finishButton");
-		
 	}
 	
 	
@@ -297,10 +376,7 @@ public class CreateFryskSessionDruid extends Dialog {
 		
 		// Process previous page data
 		int page = this.notebook.getCurrentPage();
-		if (page == 1){
-			processGroupSelection = this.dataModel.dumpSelectedProcesses();
-			this.processExitListStore.populateData(processGroupSelection);
-		}
+
 		if (page == 3)
 		{
 			this.processObserverDataModel.populateInitialData(processGroupSelection);
@@ -318,8 +394,9 @@ public class CreateFryskSessionDruid extends Dialog {
 	}
 
 	private void setUpCurrentPage(){
-		int page = this.notebook.getCurrentPage();
 		
+		int page = this.notebook.getCurrentPage();
+
 		if(page == 0){
 			this.backButton.setSensitive(false);
 		}else{
