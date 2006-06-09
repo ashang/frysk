725ea6eebe49f11a5fd45b0d213bce1f40eeae84
Andrew Cagney <cagney@redhat.com>
2006-06-09 20:56:55 +0000

    Index: frysk-imports/tests/ChangeLog 2006-06-09  Andrew Cagney  <cagney@redhat.com>
    	* Makefile.am (frysk2670/gcj.sh): Use $(GCJ), not gcj; add -d
    	. and -I . to GCJ.
    	* frysk2670/ExitNotificationObserver.java: Simplify.
    	* frysk2670/AddTaskObserverAction.java: Simplify.
    	* frysk2670/TaskAction.java: Simplify.

--- frysk-imports/tests/frysk2670/ExitNotificationObserver.java
+++ frysk-imports/tests/frysk2670/ExitNotificationObserver.java
@@ -7,81 +7,35 @@ import frysk.gui.monitor.actions.TaskAction;
 import frysk.proc.Action;
 import frysk.proc.Task;
 
+public class ExitNotificationObserver
+    extends TaskTerminatingObserver
+{
 
-public class ExitNotificationObserver extends TaskTerminatingObserver {
-
-	public ExitNotificationObserver() {
-		super();
-		this.setName("Exit Notifiction Observer");
-		this.setToolTip("Notify the user that a task is about to exit\n" +
-				"and provide them the option to stop or resume the process");
-		
-		frysk.gui.monitor.actions.TaskAction myAction = new TaskAction(){
-			public void execute(Task task) {
-				if(DialogManager.showQueryDialog("Task ["+task+"] is about to exit.\n Would you like to block it")){
-					setReturnAction(Action.BLOCK);
-				}else{
-					setReturnAction(Action.CONTINUE);
-				}
-			}
-		
-			public GuiObject getCopy() {
-				return null;
-			}
-		
-			public boolean setArgument(String argument) {
-				return true;
-			}
-		
-			public String getArgument() {
-				return null;
-			}
-		
-			public ObservableLinkedList getArgumentCompletionList() {
-				return null;
-			}			
-		};
-		myAction.dontSaveObject();
-		
-		this.taskActionPoint.addAction(myAction);
-		
-	}
-	
-	public ExitNotificationObserver(ExitNotificationObserver other) {
-		super(other);
-		
-		frysk.gui.monitor.actions.TaskAction myAction = new TaskAction(){
-			public void execute(Task task) {
-				if(DialogManager.showQueryDialog("Task ["+task+"] is about to exit.\n Would you like to block it")){
-					setReturnAction(Action.BLOCK);
-				}else{
-					setReturnAction(Action.CONTINUE);
-				}
-			}
-		
-			public GuiObject getCopy() {
-				return null;
-			}
-		
-			public boolean setArgument(String argument) {
-				return true;
-			}
-		
-			public String getArgument() {
-				return null;
-			}
-		
-			public ObservableLinkedList getArgumentCompletionList() {
-				return null;
-			}			
-		};
-		myAction.dontSaveObject();
-		
-		this.taskActionPoint.addAction(myAction);
-	}
-	
-	public GuiObject getCopy(){
-		return new ExitNotificationObserver(this);
-	}
-	
+    public ExitNotificationObserver()
+    {
+	super();
+	TaskAction myAction = new TaskAction ()
+	    {
+		public void execute(Task task)
+		{
+		}
+		public GuiObject getCopy()
+		{
+		    return null;
+		}
+		public boolean setArgument(String argument)
+		{
+		    return true;
+		}
+		public String getArgument()
+		{
+		    return null;
+		}
+		public ObservableLinkedList getArgumentCompletionList()
+		{
+		    return null;
+		}			
+	    };
+	myAction.dontSaveObject();
+    }
 }
