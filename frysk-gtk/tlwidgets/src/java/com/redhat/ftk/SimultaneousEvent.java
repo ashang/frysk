52507367596eb42cc4c8633aad6684b24aff1d31
Nurdin Premji <npremji@redhat.com>
2006-07-13 20:25:35 +0000

    2006-07-13  Nurdin Premji  <npremji@redhat.com>
    	* src/java/com/redhat/ftk/SimultaneousEvent.java: Added java-gnome license
    	* src/java/com/redhat/ftk/EventPair.java: Added java-gnome license

--- frysk-gtk/tlwidgets/src/java/com/redhat/ftk/SimultaneousEvent.java
+++ frysk-gtk/tlwidgets/src/java/com/redhat/ftk/SimultaneousEvent.java
@@ -1,3 +1,11 @@
+/*
+ * Java-Gnome Bindings Library
+ *
+ * Copyright 1998-2004 the Java-Gnome Team, all rights reserved.
+ *
+ * The Java-Gnome bindings library is free software distributed under
+ * the terms of the GNU Library General Public License version 2.
+ */
 
 
 package com.redhat.ftk;
