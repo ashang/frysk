bccc2b4db07b753993674687551454abdef5d389
Adam Jocksch <ajocksch@redhat.com>
2006-07-31 19:31:23 +0000

    2006-05-31  Adam Jocksch  <ajocksch@redhat.com>
    	* tests/TestOpcodes.java (testDisassembler): Fixed, test now works
    	on x86_64.

--- frysk-imports/lib/opcodes/tests/TestOpcodes.java
+++ frysk-imports/lib/opcodes/tests/TestOpcodes.java
@@ -36,6 +36,8 @@
 // modification, you must delete this exception statement from your
 // version and license this file solely under the GPL without
 // exception.
+
+
 package lib.opcodes.tests;
 
 import inua.eio.ByteBuffer;
@@ -46,63 +48,92 @@ import junit.framework.TestCase;
 import lib.opcodes.Disassembler;
 import lib.opcodes.Instruction;
 import lib.opcodes.OpcodesException;
+import frysk.imports.Build;
 
-public class TestOpcodes extends TestCase {
+public class TestOpcodes
+    extends TestCase
+{
 
-    /**
-     * A variable that has the value true.  Used by code trying to
-     * stop the optimizer realise that there's dead code around.
-     */
-    static boolean trueXXX = true;
-    /**
-     * A function that returns true, and prints skip.  Used by test
-     * cases that want to be skipped (vis: if(broken()) return) while
-     * trying to avoid the compiler's optimizer realizing that the
-     * rest of the function is dead.
-     */
-    protected static boolean brokenXXX (int bug)
-    {
-	System.out.print ("<<BROKEN http://sourceware.org/bugzilla/show_bug.cgi?id=" + bug + " >>");
-	return trueXXX;
-    }
+  /**
+   * A variable that has the value true. Used by code trying to stop the
+   * optimizer realise that there's dead code around.
+   */
+  static boolean trueXXX = true;
 
-	/*
-	 * Note: this test is expected to fail on anything but i386 for the time being.
-	 * TODO: come up with a way of doing the correct assertEquals for other archs
-	 */
-	public void testDisassembler(){
+  /**
+   * A function that returns true, and prints skip. Used by test cases that want
+   * to be skipped (vis: if(broken()) return) while trying to avoid the
+   * compiler's optimizer realizing that the rest of the function is dead.
+   */
+  protected static boolean brokenXXX (int bug)
+  {
+    System.out.print("<<BROKEN http://sourceware.org/bugzilla/show_bug.cgi?id="
+                     + bug + " >>");
+    return trueXXX;
+  }
 
-	  if (brokenXXX(2712))
-	    return;
+  /*
+   * Note: this test is expected to fail on anything but i386 for the time
+   * being. TODO: come up with a way of doing the correct assertEquals for other
+   * archs
+   */
+  public void testDisassembler ()
+  {
 
-		ByteBuffer buffer = new DummyByteBuffer();
-		final int numInstructions = 16;
-		
-		Disassembler disAsm = new Disassembler(buffer);
-		LinkedList list = null;
-		try{
-			list = disAsm.disassembleInstructions(0, numInstructions);
-		}
-		catch(OpcodesException e){
-			e.printStackTrace(System.err);
-			fail("Exception thrown during disassembly");
-		}
-		
-		assertNotNull(list);
-		assertEquals(list.size(), numInstructions);
-		
-		String[] addrs = {"0", "2", "4", "6", "7", "8", "a", "c", "e", "f",
-				"10", "12", "14", "16", "17", "18"};
-		String[] insts = {"DWORD PTR [ecx]", "BYTE PTR [ebx]", "0x5", "es", "es", "al",
-				"BYTE PTR [ebx]", "0x5", "es", "es", "al",
-				"BYTE PTR [ebx]", "0x5", "es", "es", "al"};
-		
-		for(int i = 0; i < list.size(); i++){
-			Instruction inst = (Instruction) list.get(i);
-			assertNotNull(inst);
-			
-			assertEquals(addrs[i], Long.toHexString(inst.address));
-			assertEquals(insts[i], inst.instruction);
-		}
-	}
+    // if (brokenXXX(2712))
+    // return;
+
+    ByteBuffer buffer = new DummyByteBuffer();
+    final int numInstructions = 16;
+
+    Disassembler disAsm = new Disassembler(buffer);
+    LinkedList list = null;
+    try
+      {
+        list = disAsm.disassembleInstructions(0, numInstructions);
+      }
+    catch (OpcodesException e)
+      {
+        e.printStackTrace(System.err);
+        fail("Exception thrown during disassembly");
+      }
+
+    assertNotNull(list);
+    assertEquals(list.size(), numInstructions);
+
+    String[] addrs = { "0", "2", "4", "6", "7", "8", "a", "c", "e", "f", "10",
+                      "12", "14", "16", "17", "18" };
+
+    String[] insts;
+
+    boolean is64 = Build.BUILD_ARCH.indexOf("_64") != - 1; 
+    
+    if (!is64)
+      {
+        insts = new String[] { "DWORD PTR [ecx]", "BYTE PTR [ebx]", "0x5",
+                              "es", "es", "al", "BYTE PTR [ebx]", "0x5", "es",
+                              "es", "al", "BYTE PTR [ebx]", "0x5", "es", "es",
+                              "al" };
+
+      }
+    else
+      {
+        insts = new String[] { "DWORD PTR [rcx]", "BYTE PTR [rbx]", "0x5",
+                              "(bad)  ", "(bad)  ", "al", "BYTE PTR [rbx]",
+                              "0x5", "(bad)  ", "(bad)  ", "al" };
+      }
+
+    assertNotNull(insts);
+
+    int instCount = is64? 11: 16;
+    
+    for (int i = 0; i < instCount; i++)
+      {
+        Instruction inst = (Instruction) list.get(i);
+        assertNotNull(inst);
+        
+        assertEquals(addrs[i], Long.toHexString(inst.address));
+        assertEquals(insts[i], inst.instruction);
+      }
+  }
 }
