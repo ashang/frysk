a60fea0f1f3bd6c29c53cff7d5b7b73b0bd0bc2f
Phil Muldoon <pmuldoon@redhat.com>
2006-09-13 21:10:28 +0000

    2006-09-13  Phil Muldoon  <pmuldoon@redhat.com>
    	* sessions/SessionManager.java: Cleaned up. Added Java Doc

--- frysk-gui/frysk/gui/sessions/SessionManager.java
+++ frysk-gui/frysk/gui/sessions/SessionManager.java
@@ -37,7 +37,6 @@
 // version and license this file solely under the GPL without
 // exception.
 
-
 package frysk.gui.sessions;
 
 import java.io.File;
@@ -49,98 +48,126 @@ import frysk.Config;
 import frysk.gui.monitor.ObjectFactory;
 import frysk.gui.monitor.ObservableLinkedList;
 import frysk.gui.monitor.UniqueHashMap;
+/**
+ * 
+ * @author pmuldoon
+ *
+ */
+public class SessionManager {
 
-public class SessionManager
-{
+	public static SessionManager theManager = new SessionManager();
 
-  public static SessionManager theManager = new SessionManager();
+	ObservableLinkedList sessions = new ObservableLinkedList();
 
-  ObservableLinkedList sessions = new ObservableLinkedList();
+	private final UniqueHashMap nameHash = new UniqueHashMap();
 
-  private UniqueHashMap nameHash = new UniqueHashMap();
+	private final String SESSIONS_DIR = Config.FRYSK_DIR + "Sessions" + "/";
 
-  private final String SESSIONS_DIR = Config.FRYSK_DIR + "Sessions" + "/";
+	/**
+	 * SessionManager is a simple singleton design pattern that keeps it own
+	 * static reference. Stores a list of sessions, can load and save those sessions,
+	 * and allows addition and deletion of sessions.
+	 */
+	public SessionManager() {
+		ObjectFactory.theFactory.makeDir(SESSIONS_DIR);
+		load();
+	}
 
-  public SessionManager ()
-  {
-    ObjectFactory.theFactory.makeDir(SESSIONS_DIR);
-    this.load();
-  }
+	/**
+	 * Add a session to the manager
+	 * @param session - the session to be added.
+	 */
+	public void addSession(final Session session) {
+		nameHash.add(session);
+		sessions.add(session);
+	}
 
-  public ObservableLinkedList getSessions ()
-  {
-    return this.sessions;
-  }
+	/**
+	 * Clear all sessions from the manager. Will not delete
+	 * or alter the sessions themselves.
+	 */
+	public void clear() {
+		nameHash.clear();
+		sessions.clear();
+	}
 
-  public boolean nameIsUsed (String name)
-  {
-    return this.nameHash.nameIsUsed(name);
-  }
+	/**
+	 * Returns a session object via the name of the session 
+	 * 
+	 * @param name - the name of the session to lookup
+	 * @return Session - the session found by the lookup.
+	 */
+	public Session getSessionByName(final String name) {
+		return (Session) nameHash.get(name);
+	}
 
-  public void addSession (Session session)
-  {
-    this.nameHash.add(session);
-    this.sessions.add(session);
-  }
+	/**
+	 * Returns a list of all sessions the manager
+	 * has knowledge about.
+	 * 
+	 * @return ObservableLinkedList of sessions.
+	 */
+	public ObservableLinkedList getSessions() {
+		return sessions;
+	}
 
-  public void removeSession (Session session)
-  {
-    ObjectFactory.theFactory.deleteNode(SESSIONS_DIR + session.getName());
-    this.nameHash.remove(session);
-    this.sessions.remove(session);
-  }
 
-  public void save ()
-  {
-    Iterator iterator = this.getSessions().iterator();
-    while (iterator.hasNext())
-      {
-        Session session = (Session) iterator.next();
-        if (session.shouldSaveObject())
-          {
-            Element node = new Element("Session");
-            ObjectFactory.theFactory.saveObject(session, node);
-            ObjectFactory.theFactory.exportNode(SESSIONS_DIR
-                                                + session.getName(), node);
-          }
-      }
-  }
+	/**
+	 * Determine if there already is a session in the manager
+	 * via the name of that session.
+	 * 
+	 * @param name - the name of that session.
+	 * @return boolean, true if found.
+	 */
+	public boolean nameIsUsed(final String name) {
+		return nameHash.nameIsUsed(name);
+	}
 
-  public void clear ()
-  {
-    this.nameHash.clear();
-    this.sessions.clear();
-  }
+	/**
+	 * Remove a session from the manager. This will delete the session
+	 * from disk, also.
+	 * 
+	 * @param session - the session object to remove.
+	 */
+	public void removeSession(final Session session) {
+		ObjectFactory.theFactory.deleteNode(SESSIONS_DIR + session.getName());
+		nameHash.remove(session);
+		sessions.remove(session);
+	}
 
-  public void load ()
-  {
-    clear();
-    Element node = new Element("Session");
-    File sessionsDir = new File(this.SESSIONS_DIR);
-    String[] array = sessionsDir.list();
-    Session loadedSession = null;
-    for (int i = 0; i < array.length; i++)
-      {
-        if (array[i].startsWith("."))
-          {
-            continue;
-          }
-        try
-          {
-            node = ObjectFactory.theFactory.importNode(SESSIONS_DIR + array[i]);
-            loadedSession = (Session) ObjectFactory.theFactory.loadObject(node);
-          }
-        catch (Exception e)
-          {
-            continue;
-          }
-        this.addSession(loadedSession);
-      }
+	public void save() {
+		final Iterator iterator = getSessions().iterator();
+		while (iterator.hasNext()) {
+			final Session session = (Session) iterator.next();
+			if (session.shouldSaveObject()) {
+				final Element node = new Element("Session");
+				ObjectFactory.theFactory.saveObject(session, node);
+				ObjectFactory.theFactory.exportNode(SESSIONS_DIR
+						+ session.getName(), node);
+			}
+		}
+	}
+	
+	public void load() {
+		clear();
+		Element node = new Element("Session");
+		final File sessionsDir = new File(SESSIONS_DIR);
+		final String[] array = sessionsDir.list();
+		Session loadedSession = null;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i].startsWith(".")) {
+				continue;
+			}
+			try {
+				node = ObjectFactory.theFactory.importNode(SESSIONS_DIR
+						+ array[i]);
+				loadedSession = (Session) ObjectFactory.theFactory
+						.loadObject(node);
+			} catch (final Exception e) {
+				continue;
+			}
+			addSession(loadedSession);
+		}
 
-  }
-
-  public Session getSessionByName (String name)
-  {
-    return (Session) this.nameHash.get(name);
-  }
+	}
 }
