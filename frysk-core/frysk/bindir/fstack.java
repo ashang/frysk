cd4e12709938ab0ababe7c7b04f80016eb2378e8
Nurdin Premji <npremji@redhat.com>
2006-10-02 19:54:04 +0000

    frysk-core/CL         * frysk/bindir/fstack.java: Added command line parser + logging options.         Changed to print tasks in ascending order of TID.         (handleTask) changed to (storeTask)

--- frysk-core/frysk/bindir/fstack.java
+++ frysk-core/frysk/bindir/fstack.java
@@ -37,8 +37,18 @@
 // version and license this file solely under the GPL without
 // exception.
 
+//package frysk.bindir;
+
 import inua.util.PrintWriter;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import frysk.EventLogger;
 import frysk.proc.Manager;
 import frysk.proc.Proc;
 import frysk.proc.ProcId;
@@ -48,47 +58,93 @@ import frysk.proc.Task;
 import frysk.proc.TaskException;
 import frysk.rt.StackFactory;
 import frysk.rt.StackFrame;
+import gnu.classpath.tools.getopt.FileArgumentCallback;
+import gnu.classpath.tools.getopt.Option;
+import gnu.classpath.tools.getopt.OptionException;
+import gnu.classpath.tools.getopt.Parser;
 
-class fstack
+public class fstack
 {
   private PrintWriter writer;
+  private TreeMap sortedTasks;
 
   private Proc proc;
 
-  private int pid;
+  private static int pid = 0;
 
   private ProcTasksObserver procTasksObserver;
 
-  public static void main (String[] args)
-  {
-    
-    fstack stacker = new fstack();
-    stacker.setWriter(new PrintWriter(System.out, true));
-    stacker.run(args);
-  }
+  private static Parser parser;
 
-  private void setWriter (PrintWriter writer)
+  protected static final Logger logger = EventLogger.get("logs/",
+                                                         "frysk_core_event.log");
+
+  private static String levelValue;
+
+  private static Level level;
+
+  public void setWriter (PrintWriter writer)
   {
     this.writer = writer;
   }
 
-  private void run (String[] args)
-  {    
-    // Logger logger = EventLogger.get ("logs/", "frysk_core_event.log");
-    // Handler handler = new ConsoleHandler ();
-    // handler.setLevel(Level.FINEST);
-    // logger.addHandler(handler);
-    // logger.setLevel(Level.ALL);
-    // LogManager.getLogManager().addLogger(logger);
-
-    // FIXME: Implement option parsing, classpath option parser stuff.
-    if (args.length == 0)
+  private static void addOptions (Parser parser)
+  {
+    parser.add(new Option(
+                          "console",
+                          'c',
+                          "Set the console level. The console-level can be "
+                              + "[ OFF | SEVERE | WARNING | INFO | CONFIG | FINE | FINER | FINEST]",
+                          "<console-level>")
+    {
+      public void parsed (String consoleValue) throws OptionException
       {
-        writer.println("Usage: fstack <pid>");
-        return;
+        try
+          {
+            Level consoleLevel = Level.parse(consoleValue);
+            // Need to set both the console and the main logger as
+            // otherwize the console won't see the log messages.
+
+            System.out.println("console " + consoleLevel);
+            Handler consoleHandler = new ConsoleHandler();
+            consoleHandler.setLevel(consoleLevel);
+            logger.addHandler(consoleHandler);
+            logger.setLevel(consoleLevel);
+            System.out.println(consoleHandler);
+
+          }
+        catch (IllegalArgumentException e)
+          {
+            throw new OptionException("Invalid log console: " + consoleValue);
+          }
+
       }
-    // Get the process from a pid.
-    pid = Integer.parseInt(args[0]);
+    });
+    parser.add(new Option(
+                          "level",
+                          'l',
+                          "Set the log level. The log-level can be "
+                              + "[ OFF | SEVERE | WARNING | INFO | CONFIG | FINE | FINER | FINEST]",
+                          "<log-level>")
+    {
+      public void parsed (String arg0) throws OptionException
+      {
+        levelValue = arg0;
+        try
+          {
+            level = Level.parse(levelValue);
+            logger.setLevel(level);
+          }
+        catch (IllegalArgumentException e)
+          {
+            throw new OptionException("Invalid log level: " + levelValue);
+          }
+      }
+    });
+  }
+
+  public void run (int pid)
+  {
     Manager.host.requestRefreshXXX(true);
 
     // XXX: Should get a message back when the refresh has finished and the
@@ -96,43 +152,49 @@ class fstack
     Manager.eventLoop.runPending();
     proc = Manager.host.getProc(new ProcId(pid));
 
-    if (null == proc) {
-	System.out.println("Couldn't get the proc");
-	System.exit(1);
-}
+    if (null == proc)
+      {
+        System.out.println("Couldn't get the proc");
+        System.exit(1);
+      }
 
-    procTasksObserver = new ProcTasksObserver(proc, new StackTasksObserver(proc));
+    procTasksObserver = new ProcTasksObserver(proc,
+                                              new StackTasksObserver(proc));
     Manager.eventLoop.start();
-  }  
-
-  private final  void removeObservers(Task task) 
-  {
-	task.requestDeleteClonedObserver(procTasksObserver);
-	//task.requestDeleteTerminatedObserver(procTasksObserver);
   }
 
-  public final void handleTask (Task task, PrintWriter writer)
+  private final void removeObservers (Task task)
+  {
+    task.requestDeleteClonedObserver(procTasksObserver);
+    // task.requestDeleteTerminatedObserver(procTasksObserver);
+  }
+
+  public final void storeTask (Task task)
   {
     if (null != task)
-      {
-        // writer.println("Stack trace for task " + task);
-        writer.println("Task #"+ task.getTid());
+      {  
         try
           {
+            LinkedList list = new LinkedList();
+		list.add(new String("Task #" + task.getTid()));
             int count = 0;
             for (StackFrame frame = StackFactory.createStackFrame(task); frame != null; frame = frame.getOuter())
               {
                 // FIXME: do valgrind-like '=== PID ===' ?
                 String output = "#" + count + " 0x"
-                + Long.toHexString(frame.getAddress()) + " in "
-                + frame.getMethodName() + " ()";
-                
+                                + Long.toHexString(frame.getAddress()) + " in "
+                                + frame.getMethodName() + " ()";
+
                 if (frame.getSourceFile() != null)
                   output = output + " from " + frame.getSourceFile();
                 
-                writer.println(output);
+                list.add(output);
                 count++;
               }
+
+		if (null == sortedTasks)
+			sortedTasks = new TreeMap();
+            sortedTasks.put(task.getName(), list);
           }
         catch (TaskException _)
           {
@@ -142,6 +204,23 @@ class fstack
       }
   }
 
+  public final void printTasks () 
+  {
+    Iterator iter = sortedTasks.values().iterator();
+    while (iter.hasNext())
+    {      
+      LinkedList output = (LinkedList) iter.next();
+      Iterator i = output.iterator();
+      while (i.hasNext())
+        {
+          String s = (String) i.next();
+          writer.println(s);
+        }
+    }
+    
+  
+  }
+  
   private class StackTasksObserver
       implements ProcObserver.ProcTasks
   {
@@ -154,38 +233,40 @@ class fstack
 
     public void existingTask (Task task)
     {
-      
-      // Print the stack frame for this stack.
-      handleTask(task, writer);    
-// Remove this task from the list of tasks.
-      if (taskList.contains(task)) {
-        taskList.remove(task);
-       }
 
+      // Print the stack frame for this stack.
+      storeTask(task);
+      // Remove this task from the list of tasks.
+      if (taskList.contains(task))
+        {
+          taskList.remove(task);
+        }
 
       if (0 == taskList.size())
         {
-           removeObservers(task);
-        }  
-      
+          //Print all the tasks in order.
+          printTasks();
+          //Remove the observer from this proc.
+          removeObservers(task);
+    	}
     }
-
     public void taskAdded (Task task)
     {
       // TODO Auto-generated method stub
 
-	//Bonus task:
-	handleTask(task, writer);
-	removeObservers(task);
+      // Bonus task:
+      storeTask(task);
+      removeObservers(task);
 
-	if (taskList.contains(task)) {
-		taskList.remove(task);
-	}
+      if (taskList.contains(task))
+        {
+          taskList.remove(task);
+        }
 
-	if (0 == taskList.size())
-	{
-		removeObservers(task);
-	}
+      if (0 == taskList.size())
+        {
+          removeObservers(task);
+        }
     }
 
     public void taskRemoved (Task task)
@@ -194,20 +275,20 @@ class fstack
       if (taskList.contains(task))
         {
           System.out.println("Task was removed before stack trace could occur");
-	  
+
           taskList.remove(task);
         }
 
-	if (0  == taskList.size())
-	  {
-	     removeObservers(task);
-	  }
+      if (0 == taskList.size())
+        {
+          removeObservers(task);
+        }
 
-	//Should I be able to print a stack frame here?
-	  handleTask(task, writer);
+      // Should I be able to print a stack frame here?
+      storeTask(task);
 
-	//Do I need to remove the observer from a dead task?
-	removeObservers(task);
+      // Do I need to remove the observer from a dead task?
+      removeObservers(task);
     }
 
     public void addFailed (Object observable, Throwable w)
@@ -225,9 +306,57 @@ class fstack
     public void deletedFrom (Object observable)
     {
       // TODO Auto-generated method stub
-	System.out.println("Shutting down");
-	Manager.eventLoop.requestStop();
+      Manager.eventLoop.requestStop();
     }
 
-  }  
+  }
+
+  public static void main (String[] args)
+  {
+
+    parser = new Parser("fstack", "1.0", true)
+    {
+      protected void validate() throws OptionException{
+        if (0 == pid) {
+          throw new OptionException ("no pid provided");
+        }
+      }
+    };
+    addOptions(parser);
+    parser.setHeader("Usage: fstack <PID>");
+
+    parser.parse(args, new FileArgumentCallback()
+    {
+      public void notifyFile (String arg) throws OptionException
+      {
+        try
+          {
+            if (0 == pid)
+              {
+                pid = Integer.parseInt(arg);
+              }
+            else
+              {
+                throw new OptionException("too many pids");
+              }
+
+          }
+        catch (Exception _)
+          {
+            throw new OptionException("couldn't parse pid");
+          }
+      }
+    });
+
+    if (null != levelValue)
+      {
+        logger.setLevel(level);
+      }
+
+    fstack stacker = new fstack();
+
+    stacker.setWriter(new PrintWriter(System.out, true));
+    stacker.run(pid);
+
+  }
 }
