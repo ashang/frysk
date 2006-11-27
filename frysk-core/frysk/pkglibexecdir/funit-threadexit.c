26e6f86287702a6b8caa363f59fac3d27897ae50
Andrew Cagney <cagney@redhat.com>
2006-11-27 20:47:38 +0000

    Index: frysk-core/frysk/pkglibexecdir/ChangeLog 2006-11-27  Andrew Cagney  <cagney@redhat.com>
    	* funit-threadexit.c (scan_thread): Delete
    	(main): Do the scan for thread in 'X' state here, instead of in
    	scan_thread.  Create only one thread.
    	(condition_cond, condition_mutex): Delete.
    	(thread_running_barrier): Rename "barrier".
    	(thread_id): Make volatile.
    	(op_thread): Simplify, use only one barrier.

--- frysk-core/frysk/pkglibexecdir/funit-threadexit.c
+++ frysk-core/frysk/pkglibexecdir/funit-threadexit.c
@@ -18,101 +18,26 @@ void
 usage ()
 {
   printf ("\
-Usage:\n\
+Usage: <pid> <sig> <timeout>\n\
 Where:\n\
-    <pid> <sig> <sec>  Manager process to signal, signal to use once\n\
-                     a thread completes, and how long to wait to join\n\
-                     the thread.\n\
+    <pid> <sig>    Notify <pid> with <sig> once thread has exited.\n\
+    <timeout>      Terminate program after <timeout>\n\
+Creates a thread that exits but isn't joined, putting it into the X state.\n\
 ");
-  _exit (1);
+  exit (1);
 }
 
-static pthread_barrier_t barrier;
-pthread_cond_t  condition_cond  = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t condition_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-pthread_t thread_id = 0;
+static pthread_barrier_t thread_running_barrier;
+volatile pthread_t thread_id = 0;
 
 void *
 op_thread (void *np)
 {
   thread_id = gettid ();
-
+  trace ("thread creation: TTID=%d", (int)thread_id);
   // Make sure scan_thread gets the thread_id
-  pthread_mutex_lock( &condition_mutex );
-  pthread_cond_signal( &condition_cond );
-  pthread_mutex_unlock( &condition_mutex );
-
-  while (thread_id != 0) {
-    pthread_mutex_lock( &condition_mutex );
-    pthread_cond_wait( &condition_cond, &condition_mutex );
-    pthread_mutex_unlock( &condition_mutex );
-  }
-
-  trace ("thread creation: PID=%d TTID=%d", (int)*((pthread_t*)np), (int)thread_id);
+  pthread_barrier_wait (&thread_running_barrier);
   return NULL;
-}
-
-/* Scan /proc for state of thread_id.  */
-
-void *
-scan_thread (void *thread_status)
-{
-  int fd = 0;
-  char buf [1024];
-  char buffer [1024];
-  char state;
-  struct stat statbuf;
-
-  // Wait for the thread_id from op_thread
-  while (thread_id == 0) {
-    pthread_mutex_lock( &condition_mutex );
-    pthread_cond_wait( &condition_cond, &condition_mutex );
-    pthread_mutex_unlock( &condition_mutex );
-  }
-
-  *((int*)thread_status) = 0;
-
-  sprintf ((char*)&buf, "/proc/%d/task/%d/stat", (int) getpid(), (int) thread_id);
-  OK (stat,(buf, &statbuf));
-  
-  errno = 0;
-  fd = open (buf, O_RDONLY);
-  if (errno != 0) {
-    perror ("opening");
-    return 0;
-  }
-
-  pthread_mutex_lock( &condition_mutex );
-  thread_id = 0;
-  pthread_cond_signal( &condition_cond );
-  pthread_mutex_unlock( &condition_mutex );
-
-  while (1)    
-    {
-      lseek (fd, 0, SEEK_SET);
-      if (errno != 0) {
-	perror ("seeking");
-	break;
-      }
-      read (fd, buffer, sizeof (buffer));
-      if (errno != 0) {
-	perror ("reading");
-	break;
-      }
-      char* commEnd = strchr (buffer, ')');
-      state = buffer[(long)(commEnd - buffer + (void*)2)];
-
-      if (state == 'X') {
-	trace ("thread termination: tid=%d state=X", (int)thread_id);
-	*((int*)thread_status) = 1;
-	break;
-      }
-    }
-
-  close (fd);
-  pthread_barrier_wait (&barrier);
-  return 0;
 }
 
 int
@@ -122,48 +47,61 @@ main (int argc, char *argv[])
   int pid;
   int sec;
   pthread_t p;
-  pthread_t q;
-  pthread_t threads;
-  pthread_attr_t pthread_attr;
-  int thread_status;
 
   if (argc < 4)
     usage ();
   pid = atol (argv[1]);
   sig = atol (argv[2]);
   sec = atol (argv[3]);
+  trace ("PID=%d SIG=%d TIMEOUT=%d", pid, sig, sec);
 
+  // Create a thread that (almost) immediatly exits, synconronize with
+  // it to ensure that the global THREAD_ID was set.
+  pthread_attr_t pthread_attr;
   pthread_attr_init (&pthread_attr);
-  pthread_attr_setstacksize (&pthread_attr, PTHREAD_STACK_MIN);
+  OK (pthread_barrier_init, (&thread_running_barrier, NULL, 2));
+  OK (pthread_create, (&p, &pthread_attr, op_thread, NULL));
+  pthread_barrier_wait (&thread_running_barrier);
 
-  trace ("PID=%d SIG=%d SEC=%d", pid, sig, sec);
-
-  //tkill (pid, sig); // ack. /* Causes race condition - removed */
-
-  threads = gettid();
-  // Synchronization barrier; need both main and the new thread.
-  OK (pthread_barrier_init, (&barrier, NULL, 2));
-  if (pthread_create (&q, &pthread_attr, scan_thread, &thread_status)) {
-    perror ("pthread_create: scan_thread");
-    _exit (1);
+  // Scan /proc for state of thread_id looking for 'X' - unjoined.
+  {
+    char *path = NULL;
+    asprintf (&path, "/proc/%d/task/%d/stat", (int) getpid(), (int) thread_id);
+    if (path == NULL)
+      pfatal ("asprintf");
+    
+    int fd = open (path, O_RDONLY);
+    if (fd <= 0)
+      pfatal ("open");
+    
+    while (1)    
+      {
+	char buffer [1024];
+	int n = pread (fd, buffer, sizeof (buffer), 0);
+	if (n <= 0) {
+	  // On FC-6 the thread completly disappears from /proc.
+	  if (errno == ESRCH) {
+	    perror ("pread");
+	    break;
+	  }
+	  pfatal ("pread");
+	}
+	char* commEnd = strchr (buffer, ')');
+	char state = buffer[(long)(commEnd - buffer + (void*)2)];
+	trace ("pread returns %d, state=%c", n, state);
+	if (state == 'X') {
+	  trace ("thread termination: tid=%d state=X", (int)thread_id);
+	  break;
+	}
+	usleep (1000 * 100); // 10th of a second, hopefully
+      }
+    close (fd);
   }
 
-  if (pthread_create (&p, &pthread_attr, op_thread, &threads)) {
-    perror ("pthread_create: op_thread");
-    _exit (1);
-  }
-  pthread_barrier_wait (&barrier);
-  OK (pthread_join, (q, NULL));
-
-  if (thread_status)
-    tkill (pid, sig); // ack.
-  else
-    abort ();
-
-  printf ("%d\n", thread_status);
+  // Tell the parent that the thread has reached 'X' state.
+  tkill (pid, sig); // ack.
   sleep (sec);
-
+  
   OK (pthread_join, (p, NULL));
-
   return 0;
 }
