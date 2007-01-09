498e0a3b61e6011dbf413af7421e50c7b7408794
Andrew Cagney <cagney@redhat.com>
2007-01-09 12:05:13 +0000

    Index: frysk-core/frysk/dom/ChangeLog 2007-01-09  Andrew Cagney  <cagney@redhat.com>
    	* TestParser.java (NEWDOM, tearDown, tmpFile, testParser): Replace
    	NEWDOM - the generated DOM's output - with a temporary file,
    	delete during tearDown.
    	(exec_path, BASEDOM, installed_list, includepaths, sourcelist)
    	(root, data, dom, IMAGE_FILENAME, CC_FILE, which_file)
    	(testParser): Re-implement testParser using List instead of
    	array's, and Paths.getExecPrefix and Paths.getDataPrefix instead
    	of Build.SRCDIR, Build.ABS_SRCDIR, and hardwired install paths.
    	(checkPath, assertDomMatchesBase): Replace checkPath with
    	assertDomMatchesBase.

--- frysk-core/frysk/dom/TestParser.java
+++ frysk-core/frysk/dom/TestParser.java
@@ -1,6 +1,6 @@
 // This file is part of the program FRYSK.
 //
-// Copyright 2006, Red Hat Inc.
+// Copyright 2006, 2007, Red Hat Inc.
 //
 // FRYSK is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
@@ -41,6 +41,7 @@
 package frysk.dom;
 
 import frysk.junit.TestCase;
+import frysk.junit.Paths;
 import frysk.dom.DOMFrysk;
 import frysk.dom.DOMImage;
 import frysk.dom.DOMSource;
@@ -48,7 +49,6 @@ import frysk.dom.DOMLine;
 import frysk.dom.DOMFactory;
 import frysk.dom.StaticParser;
 import frysk.dom.cparser.CDTParser;
-import frysk.core.Build;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -57,228 +57,147 @@ import java.io.FileReader;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 import org.jdom.Document;
-import org.jdom.Element;
 import org.jdom.output.XMLOutputter;
 
 /**
- * This is a test for the parsing capabilities. Frysk uses a snapshot of the
- * Eclipse CDT parser 2.0 which is used in standalone mode.
+ * This is a test for the parsing capabilities. Frysk uses a snapshot
+ * of the Eclipse CDT parser 2.0 which is used in standalone mode.
  */
 
 public class TestParser
     extends TestCase
 {
+    /**
+     * A scratch file for writing out the dom, if non-null it is
+     * deleted during tear-down.  This is more robust than relying on
+     * the garbage collector to delete the file during finalization.
+     */
+    private File tmpFile;
 
-  private static Element root = new Element("Parser_test");
-
-  private static Document data = new Document(root);
-
-  private static DOMFrysk dom = new DOMFrysk(data);
-
-  private static String IMAGE_FILENAME = "Task 28428";
-
-  private static String CC_FILE = "./test";
-
-  private static String[] sourcelist = null;
-
-  private static String installed_list = "/usr/share/frysk/samples/test_looper.c";
-
-  private static String[] includepaths;
-
-  private static String[] exec_path = {
-                                       "./frysk/gui/srcwin/testfiles/test_main_looper",
-                                       "/usr/share/frysk/samples/test_main_looper" };
-
-  private static String[] NEWDOM = {
-                                    "./frysk/gui/srcwin/testfiles/new_dom.xml",
-                                    "/usr/share/frysk/samples/new_dom.xml" };
-
-  private static String[] BASEDOM = {
-                                     Build.SRCDIR + "/frysk/gui/srcwin/testfiles/test_looper.xml",
-                                     "/usr/share/frysk/samples/test_looper.xml" };
-
-  private static int which_file;
-
-  public static void testParser ()
-  {
-      if (brokenXXX (3841))
+    public void testParser ()
+	throws IOException
+    {
+	// XXX: The current problem is that the relevant paths are
+	// pointing into the build tree.
+	if (brokenXXX (3841))
 	  return;
 
+	String IMAGE_FILENAME = "Task 28428";
+	String CC_FILE = "./test";
 
-    if (DOMFactory.pathFound(Build.SRCDIR))
-      {
-        // Figure out whether we are doing an in-tree check or an installed check
-        // which_file = 0 means in-tree, = 1 means an installed check
-        which_file = 0;
-        ArrayList arraysourcelist = DOMFactory.getSrcFiles(exec_path[which_file]);
-        assertTrue(arraysourcelist.size() != 0);
-        // Convert ArrayList to String Array
-        sourcelist = (String[]) arraysourcelist.toArray(new String [arraysourcelist.size()]);
-      }
-    else
-      {
-        which_file = 1;
-        assertTrue("testing source file existence",
-                   (DOMFactory.pathFound(installed_list)));
-      }
-    
-    // if this array is length 0, we must be doing an "installed" test
-    if (sourcelist.length == 0)
-      {
-        sourcelist = new String[1];
-        sourcelist[0] = installed_list;
-      }
+	String execPath = Paths.getExecPrefix () + "/test_main_looper";
+	List sources = DOMFactory.getSrcFiles (execPath);
+	assertTrue ("sources.size > 0", sources.size () > 0);
+	
+	// Get the list of include file paths associated with this
+	// image
+	String[] includePaths = (String[])
+	    DOMFactory.getIncludePaths(execPath).toArray (new String[0]);
 
-    // Get the list of include file paths associated with this image
-    ArrayList arrayincludepaths = DOMFactory.getIncludePaths(exec_path[which_file]);
-    includepaths = (String[]) arrayincludepaths.toArray(new String [arrayincludepaths.size()]);
-
-    dom = new DOMFrysk("TaskTask");
-    dom.addImage(IMAGE_FILENAME, CC_FILE, CC_FILE);
-    DOMImage image = dom.getImage(IMAGE_FILENAME);
-    for (int i = 0; i < sourcelist.length; i++)
-      {
-        String filename = sourcelist[i].substring(sourcelist[i].lastIndexOf("/") + 1);
-        String path = sourcelist[i].substring(0, sourcelist[i].lastIndexOf("/"));
-        DOMSource source = new DOMSource(filename, path, includepaths);
-        // Read the file lines from disk
-        // XXX: Remote file access?
-        try
-          {
-            BufferedReader reader = new BufferedReader(
-                                                       new FileReader(
-                                                                      new File(
-                                                                               sourcelist[i])));
-            int offset = 0;
-            int lineNum = 0;
-
-            while (reader.ready())
-              {
-                String text = reader.readLine();
-                // XXX: detect executable lines?
-                DOMLine l = new DOMLine(lineNum++, text + "\n", offset, false,
-                                        false, Long.parseLong("deadbeef", 16));
-                source.addLine(l);
-
-                offset += text.length() + 1;
-              }
-            image.addSource(source);
-
-            // Parse the file and populate the DOM
-            StaticParser parser = new CDTParser();
-            parser.parse(dom, source, image);
-
-          }
-        catch (IOException e)
-          {
-            System.out.println("Error reading source file " + sourcelist[i]
-                               + "\nerror = " + e.getMessage());
-          }
-        i++;
-      }
-
-    try
-      {
-        // Make sure there is not a file already there is this test has been run already
-        checkPath(NEWDOM[which_file]);
-        BufferedWriter out = new BufferedWriter(new FileWriter(NEWDOM[which_file]));
-        writeDOM(dom, out);
-      }
-    catch (IOException e)
-      {
-        e.printStackTrace();
-      }
-  }
-
-  /*
-   * write out the DOM @param dom is the current DOM just created @param out is
-   * a Buffered
-   * 
-   * @param dom is the DOMFrysk dom to write out
-   * @param out is the output stream to write the dom to
-   */
-
-  public static void writeDOM (DOMFrysk dom, BufferedWriter out)
-  {
-    Document doc = dom.getDOMFrysk();
-    try
-      {
-        XMLOutputter serializer = new XMLOutputter();
-        serializer.getFormat();
-        serializer.output(doc, out);
-      }
-    catch (IOException e)
-      {
-        System.err.println(e);
-      }
-    
-    try
-      {
-        assertTrue("testing DOM files comparison", compareFiles(NEWDOM[which_file], 
-                                                                BASEDOM[which_file]));
-      }
-    catch (IOException e)
-      {
-        e.printStackTrace();
-        assertTrue("IO Error on opening the DOM files", false);
-      }
-  }
-
-  /**
-   * checkPath checks to see if a new DOM has already been created and
-   * if it has, delete it
-   * 
-   * @param path is a String containing the path to check
-   */
-  public static void checkPath (String path)
-  {
-    File f = new File(path);
-    if (f.exists())
-      f.delete();
-    return;
-  }
-
-  /**
-   * compareFiles checks to see if the generated DOM matches the previously
-   * generated DOM
-   * 
-   * @param newfile is a String containing the path to the new dom just created
-   * @param basefile is a String containing the path to the base dom to compare with
-   */
-  public static boolean compareFiles(String newfile, String basefile) throws IOException
-  {
-    File new_dom_input = new File(newfile);
-    File base_dom_input = new File(basefile);
-    
-    FileInputStream base = new FileInputStream(new_dom_input);
-    FileInputStream newdom = new FileInputStream(base_dom_input);
-    
-    try
-    {
-        byte[] lbuffer = new byte[4096];
-        byte[] rbuffer = new byte[lbuffer.length];
-        for (int lcount = 0; (lcount = newdom.read(lbuffer)) > 0;)
-        {
-            int bytesRead = 0;
-            for (int rcount = 0; (rcount = base.read(rbuffer, bytesRead, lcount - bytesRead)) > 0;)
-            {
-                bytesRead += rcount;
-            }
-            for (int byteIndex = 0; byteIndex < lcount; byteIndex++)
-            {
-                if (lbuffer[byteIndex] != rbuffer[byteIndex])
-                    return false;
-            }
-        }
+	DOMFrysk dom = new DOMFrysk ("TaskTask");
+	dom.addImage (IMAGE_FILENAME, CC_FILE, CC_FILE);
+	DOMImage image = dom.getImage (IMAGE_FILENAME);
+	for (Iterator i = sources.iterator (); i.hasNext (); ) {
+	    String filename = (String) i.next ();
+	    File file = new File (filename);
+	    // XXX: Use file and its parser here
+	    String basename = filename.substring(filename.lastIndexOf("/") + 1);
+	    String path = filename.substring(0, filename.lastIndexOf("/"));
+	    DOMSource source = new DOMSource (basename, path, includePaths);
+	    // Read the file lines from disk XXX: Remote file access?
+	    BufferedReader reader = new BufferedReader (new FileReader(file));
+	    int offset = 0;
+	    int lineNum = 0;
+	    
+	    while (reader.ready()) {
+		String text = reader.readLine();
+		// XXX: detect executable lines?
+		DOMLine l = new DOMLine(lineNum++, text + "\n", offset, false,
+					false, Long.parseLong("deadbeef", 16));
+		source.addLine(l);
+		
+		offset += text.length() + 1;
+	    }
+	    image.addSource(source);
+		
+	    // Parse the file and populate the DOM
+	    StaticParser parser = new CDTParser();
+	    parser.parse(dom, source, image);
+	}
+	 
+	// Make sure there is not a file already there is this test
+	// has been run already
+	String name = getClass ().getName ();
+	File pwd = new File (".");
+	tmpFile = File.createTempFile (name + ".", ".tmp", pwd);
+	BufferedWriter out = new BufferedWriter (new FileWriter (tmpFile));
+	writeDOM(dom, out);
+	assertDomMatchesBase (tmpFile);
     }
-    finally
+
+    public void tearDown ()
     {
-        base.close();
-        newdom.close();
+	if (tmpFile != null) {
+	    tmpFile.delete ();
+	    tmpFile = null;
+	}
     }
-    return true;
-  }
+
+    /*
+     * write out the DOM @param dom is the current DOM just created
+     * @param out is a Buffered
+     * 
+     * @param dom is the DOMFrysk dom to write out
+     * @param out is the output stream to write the dom to
+     */
+    public static void writeDOM (DOMFrysk dom, BufferedWriter out)
+	throws IOException
+    {
+	Document doc = dom.getDOMFrysk();
+	XMLOutputter serializer = new XMLOutputter();
+	serializer.getFormat();
+	serializer.output(doc, out);
+    }
+
+    /**
+     * compareFiles checks to see if the generated DOM matches the
+     * previously generated DOM
+     * 
+     * @param newfile is a String containing the path to the new dom
+     * just created
+     */
+    public static void assertDomMatchesBase (File newfile)
+	throws IOException
+    {
+	String basedom = Paths.getDataPrefix () + "/test_looper.xml";
+	File baseDomInput = new File (basedom);
+    
+	FileInputStream base = new FileInputStream(newfile);
+	FileInputStream newdom = new FileInputStream(baseDomInput);
+    
+	try
+	    {
+		byte[] lbuffer = new byte[4096];
+		byte[] rbuffer = new byte[lbuffer.length];
+		for (int lcount = 0; (lcount = newdom.read(lbuffer)) > 0;) {
+		    int bytesRead = 0;
+		    for (int rcount = 0; (rcount = base.read(rbuffer, bytesRead, lcount - bytesRead)) > 0;) {
+			bytesRead += rcount;
+		    }
+		    for (int byteIndex = 0; byteIndex < lcount; byteIndex++) {
+			if (lbuffer[byteIndex] != rbuffer[byteIndex])
+			    fail ("Files do not match at " + byteIndex);
+		    }
+		}
+	    }
+	finally
+	    {
+		base.close();
+		newdom.close();
+	    }
+    }
 }
