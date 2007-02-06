d95851625ff223b083f4a0d0cefa2af6c1dc5eaf
Nurdin Premji <npremji@redhat.com>
2007-02-06 21:26:50 +0000

    frysk-core/frysk/rt/CL 	* TestStepping.java (testASMStepping): Fixed, was because of added brackets 	in frysk-asm.h.COMPARE_IMMED.

--- frysk-core/frysk/rt/TestStepping.java
+++ frysk-core/frysk/rt/TestStepping.java
@@ -131,9 +131,6 @@ public class TestStepping extends TestLib
   
   public void testASMStepping ()
   {
-
-    if (brokenXXX(3979))
-      return;
     if (MachineType.getMachineType() == MachineType.PPC
         || MachineType.getMachineType() == MachineType.PPC64)
       {
