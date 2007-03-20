8a4d9c97710a41e45e67885532390d9029249cdb
Jan Kratochvil <jan.kratochvil@redhat.com>
2007-03-20 12:22:50 +0000

    * kernel232837/attachstop.c: Extended for the kernel-2.6.20-1.2935.rm2.fc6.x86_64 regression.

--- frysk-imports/tests/kernel232837/attachstop.c
+++ frysk-imports/tests/kernel232837/attachstop.c
@@ -20,49 +20,28 @@ child_f (void)
 
 static pid_t child_pid;
 
+static const char *handler_state;
+
 static void
 handler (int signo)
 {
   kill (child_pid, SIGKILL);
   //  abort ();
-  fprintf (stderr, "timeout\n");
+  fprintf (stderr, "timeout: %s\n", handler_state);
   exit (1);
 }
 
 static unsigned char regs[0x10000];
 
-int main (void)
+static void attach (const char *state)
 {
   int i;
   int status;
 
-  child_pid = fork();
-  switch (child_pid)
-    {
-      case -1:
-        perror ("fork()");
-	exit (1);
-	/* NOTREACHED */
-      case 0:
-        child_f ();
-	/* NOTREACHED */
-      default:;
-	/* PASSTHRU */
-    }
-  /* Parent.  */
-
-  sleep (1);
-
+  handler_state = state;
   signal (SIGALRM, handler);
   alarm (1);
 
-  /* Buggy:
-  kernel-xen-2.6.19-1.2898.2.3.fc7.i686
-  kernel-2.6.20-1.2925.fc6.i586
-  kernel-2.6.20-1.2928.rm1.fc6.x86_64
-     Fixed:
-  kernel-2.6.20-1.2935.rm1.fc6.x86_64
-  */
   i = ptrace (PTRACE_ATTACH, child_pid, NULL, NULL);
   //assert (i == 0);
   if (i != 0) error (1, errno, "PTRACE_ATTACH");
@@ -87,18 +66,61 @@ int main (void)
   }
 
   alarm (0);
+}
+
+int main (void)
+{
+  int i;
+
+  child_pid = fork();
+  switch (child_pid)
+    {
+      case -1:
+        perror ("fork()");
+	exit (1);
+	/* NOTREACHED */
+      case 0:
+        child_f ();
+	/* NOTREACHED */
+      default:;
+	/* PASSTHRU */
+    }
+  /* Parent.  */
+
+  sleep (1);
+
+  /* Buggy:
+  kernel-xen-2.6.19-1.2898.2.3.fc7.i686
+  kernel-2.6.20-1.2925.fc6.i586
+  kernel-2.6.20-1.2928.rm1.fc6.x86_64
+     Fixed:
+  kernel-2.6.20-1.2935.rm1.fc6.x86_64
+  kernel-2.6.20-1.2935.rm2.fc6.x86_64
+  */
+  attach ("first PTRACE_ATTACH");
 
   /* Buggy:
   kernel-2.6.20-1.2935.rm1.fc6.x86_64
+     Fixed:
+  kernel-2.6.20-1.2935.rm2.fc6.x86_64
   */
   errno = 0;
   ptrace (PTRACE_PEEKUSER, child_pid, NULL, NULL);
   //assert (errno == 0);
   if (errno != 0) error (1, errno, "PTRACE_PEEKUSER");
-  ptrace (PTRACE_GETREGS, child_pid, NULL, regs);
-  //assert (errno == 0);
-  if (errno != 0) error (1, errno, "PTRACE_GETREGS");
+  i = ptrace (PTRACE_GETREGS, child_pid, NULL, regs);
+  //assert (i == 0);
+  if (i != 0) error (1, errno, "PTRACE_GETREGS");
   
+  i = ptrace (PTRACE_DETACH, child_pid, NULL, NULL);
+  //assert (i == 0);
+  if (i != 0) error (1, errno, "PTRACE_DETACH");
+
+  /* Buggy:
+  kernel-2.6.20-1.2935.rm2.fc6.x86_64
+  */
+  attach ("second PTRACE_ATTACH");
+
   //  puts ("OK");
   kill (child_pid, SIGKILL);
   //return 0;
