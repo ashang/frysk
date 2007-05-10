1511ece6471e57fadc86ec97ef3f960e121d2864
Andrew Cagney <cagney@redhat.com>
2007-05-10 23:59:10 +0000

    Index: frysk-core/frysk/proc/ChangeLog 2007-05-10  Andrew Cagney  <cagney@redhat.com>
    	* TaskId.java: Make public.

--- frysk-core/frysk/proc/TaskId.java
+++ frysk-core/frysk/proc/TaskId.java
@@ -1,6 +1,6 @@
 // This file is part of the program FRYSK.
 //
-// Copyright 2005, 2006, Red Hat Inc.
+// Copyright 2005, 2006, 2007, Red Hat Inc.
 //
 // FRYSK is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@ package frysk.proc;
  * like.  Anything wanting to search for a task should use this.
  */
 
-class TaskId
+public class TaskId
     extends Id
 {
     TaskId ()
