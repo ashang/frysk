40cade080eac60a124a5f2d14299a7e054cabdd9
Andrew Cagney <cagney@redhat.com>
2007-05-15 21:17:23 +0000

    Index: frysk-core/frysk/proc/ChangeLog 2007-05-15  Andrew Cagney  <cagney@redhat.com>
    	* Id.java (id): Make protected.

--- frysk-core/frysk/proc/Id.java
+++ frysk-core/frysk/proc/Id.java
@@ -1,6 +1,6 @@
 // This file is part of the program FRYSK.
 //
-// Copyright 2005, 2006, Red Hat Inc.
+// Copyright 2005, 2006, 2007, Red Hat Inc.
 //
 // FRYSK is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
@@ -49,7 +49,7 @@ package frysk.proc;
 class Id
     implements Comparable
 {
-    int id;
+    protected int id;
     protected Id ()
     {
 	id = -1;
