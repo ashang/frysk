4f2b858082eb0484dcf59b9304b7ea0d36216bcb
Andrew Cagney <cagney@redhat.com>
2007-06-22 20:52:47 +0000

    Index: frysk-imports/tests/ChangeLog 2007-06-22  Andrew Cagney  <cagney@redhat.com>
    	* frysk2137/Gcov.java: Add a single statement.

--- frysk-imports/tests/frysk2137/Gcov.java
+++ frysk-imports/tests/frysk2137/Gcov.java
@@ -4,5 +4,8 @@ public class Gcov
 {
     public static void main (String[] argv)
     {
+	// gcov gets grumpy when there's no code executed so offer at
+	// least one line.
+	System.out.println ("Hello World");
     }
 }
