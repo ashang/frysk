658bc3a86f7084e130788d24ebb6bd0f609b9d01
Sami Wagiaalla <swagiaal@redhat.com>
2007-06-29 17:38:39 +0000

    Index: frysk-core/frysk/pkglibdir/ChangeLog +2007-06-28  Sami Wagiaalla  <swagiaal@redhat.com> + +       * funit-stacks-values.c: Added asm volatile ("" : "+m" (varX)); +       to all the variables declarations to ensure they are not optimized +       away. +

--- frysk-core/frysk/pkglibdir/funit-stacks-values.c
+++ frysk-core/frysk/pkglibdir/funit-stacks-values.c
@@ -1,23 +1,23 @@
 #include <stdlib.h>
 
 void third(int param3){
-  int var4 = 4;
+  int var4 = 4;asm volatile ("" : "+m" (var4));
   char* c = 0;
   c[0] = var4;
 }
 
 void second(int param2){
-  int var3 = 3;
+  int var3 = 3;asm volatile ("" : "+m" (var3));
   third(var3);
 }
 
 void first(int param1){
-  int var2 = 2;
+  int var2 = 2;asm volatile ("" : "+m" (var2));
   second(var2);
 }
 
 int main(){
-  int var1 = 1;
+  int var1 = 1;asm volatile ("" : "+m" (var1));
   first (var1);
   return 0;
 }
