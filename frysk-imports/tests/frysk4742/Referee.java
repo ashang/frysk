2790edc409d9a1a185e8e2b9eb0db1070ccc29a6
Andrew Cagney <cagney@redhat.com>
2007-07-09 17:25:24 +0000

    Index: frysk-imports/tests/ChangeLog 2007-07-09  Andrew Cagney  <cagney@redhat.com>
    	* Makefile.am (frysk4742/java.sh): New rule.
    	(XFAIL_TESTS): Add.
    	* frysk4742/Referer.java: New file.
    	* frysk4742/Referee.java: New file.

---
+++ frysk-imports/tests/frysk4742/Referee.java
@@ -0,0 +1,10 @@
+package frysk4742;
+
+class Referee
+{
+    static int i;
+    int j;
+    Referee() {
+	j = i++;
+    }
+}
---
+++ frysk-imports/tests/frysk4742/Referer.java
@@ -0,0 +1,7 @@
+package frysk4742;
+
+class Referer {
+    static public void main(String[] args) {
+	new Referee();
+    }
+}
