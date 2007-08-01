d99f76ef9ff1cbb8468b1780df474ca76993757e
Nurdin Premji <npremji@redhat.com>
2007-08-01 22:31:39 +0000

    frysk-core/frysk/util/CL 	* CommandlineParser.java: Ran sun formatter.

--- frysk-core/frysk/util/CommandlineParser.java
+++ frysk-core/frysk/util/CommandlineParser.java
@@ -54,205 +54,185 @@ import frysk.EventLogger;
 import frysk.proc.ProcId;
 
 /**
- * CommandlineParser extends the getopt {@link Parser} class with common
- * options for Frysk command-line applications. It adds the
- * {@link EventLogger} options.
+ * CommandlineParser extends the getopt {@link Parser} class with common options
+ * for Frysk command-line applications. It adds the {@link EventLogger} options.
  */
-public class CommandlineParser
-{
-  Parser parser;
-  public CommandlineParser(String name, String version)
-  {
-    parser = new Parser(name, version, true);
-    
-    EventLogger.addConsoleOptions(parser);
-  }
-  
-  public CommandlineParser (String programName)
-  {
-    this(programName, Config.getVersion());
-  }
+public class CommandlineParser {
+    Parser parser;
 
-  /**
-   * Callback function. Gives an array of pids if pids were detected on the 
-   * command line.
-   * @param pids The array of pids passed on the command line.
-   */
-  public void parsePids(ProcId[] pids)
-  {
-    System.err.println("Error: Pids not supported.");
-    printHelp();
-    System.exit(1);
-  }
-  
-  /**
-   * Callback function. Gives an array of core files if core files were 
-   * detected on the command line.
-   * @param coreFiles The array of core files passed on the command line.
-   */
-  public void parseCores(Util.CoreExePair[] coreExePairs)
-  {
-    System.err.println("Error: Cores not supported.");
-    printHelp();
-    System.exit(1);
-  }
-  
-  /**
-   * Callback function. Gives a string array represented a parsed command.
-   * @param command The parsed command.
-   */
-  public void parseCommand(String[] command)
-  {
-    System.err.println("Error: Commands not supported.");
-    printHelp();
-    System.exit(1);
-  }
-  
-  public String[] parse (String[] args)
-  {
-    String[] result = doParse(args);
-    try
-      {
-        validate();
-      }
-    catch (OptionException e)
-      {
-        printHelp();
-        throw new RuntimeException(e);
-      }
-    return result;
-  }
-  
-  private String[] doParse(String[] args)
-  {
-    String[] result = parser.parse(args);
-    
-    //XXX: Should parseCommand be called with an empty array here?
-    if (result == null)
-      return null;
-    if (result.length == 0)
-      return result;
-    
-    //Check if arguments are all pids.
-    try 
-    {
-      ProcId[] pids = new ProcId[result.length];
-      pids[0] = new ProcId(Integer.parseInt(result[0]));
-      
-      for (int i = 1; i < result.length; i++)
-        {
-          try
-          {
-          pids[i] = new ProcId(Integer.parseInt(result[i]));
-          }
-          catch (NumberFormatException e)
-          {
-            throw new RuntimeException("Please don't mix pids with core files or executables");
-          }
-        }       
-      
-      parsePids(pids);      
-      return result;
+    public CommandlineParser(String name, String version) {
+	parser = new Parser(name, version, true);
+
+	EventLogger.addConsoleOptions(parser);
     }
-    catch (NumberFormatException e)
-    {
-      //Not a pid, continue on.
-    }        
-    
-    // Check if arguments are all core file/ exe file pairs..
-    if (isCoreFile(result[0]))
-      {
-        ArrayList coreExeFiles = new ArrayList();
-        
-        for (int file = 0; file < result.length - 1; file++) {
-         File coreFile;
-            if (isCoreFile(result[file])) {
-            coreFile = new File(result[file]);
-            if (isExeFile(result[file+1])) {
-              coreExeFiles.add(new Util.CoreExePair(coreFile, new File(result[file+1])));
-              file++; }
-              else
-        	 coreExeFiles.add(new Util.CoreExePair(coreFile, null)); 
-            }
-          else
-            throw new RuntimeException("Please don't mix core files with pids or executables.");         
-        }
-        
-        if (isCoreFile(result[result.length -1]))
-            coreExeFiles.add(new Util.CoreExePair(new File(result[result.length -1]), null));
-        
-        Util.CoreExePair[] coreExePairs = new Util.CoreExePair[coreExeFiles.size()];
-        System.arraycopy(coreExeFiles.toArray(), 0, coreExePairs, 0, coreExePairs.length);
-        parseCores(coreExePairs);        
-        return result;
-      }
-      
-    
-    // If not above, then this is an executable command.
-    parseCommand(result);    
-    return result;
-  }
-  
-  /**
-   * Check if the given file is a coreFile.
-   * @param fileName A file to check.
-   * @return if fileName is a corefile returns true, otherwise false.
-   */
-  private boolean isCoreFile (String fileName)
-  {
-    Elf elf;
-    try
-      {
-        elf = new Elf(fileName, ElfCommand.ELF_C_READ);
-      }
-    catch (Exception e)
-      {
-        return false;
-      }
-    
-    boolean ret = elf.getEHeader().type == ElfEHeader.PHEADER_ET_CORE;
-    elf.close();
-    return ret;
 
-  }
-  
-  private boolean isExeFile (String fileName)
-  {
-    Elf elf;
-    try
-      {
-        elf = new Elf(fileName, ElfCommand.ELF_C_READ);
-      }
-    catch (Exception e)
-      {
-        return false;
-      }
-    
-    boolean ret = elf.getEHeader().type == ElfEHeader.PHEADER_ET_EXEC;
-    elf.close();
-    return ret;
+    public CommandlineParser(String programName) {
+	this(programName, Config.getVersion());
+    }
 
-  }
+    /**
+         * Callback function. Gives an array of pids if pids were detected on
+         * the command line.
+         * 
+         * @param pids
+         *                The array of pids passed on the command line.
+         */
+    public void parsePids(ProcId[] pids) {
+	System.err.println("Error: Pids not supported.");
+	printHelp();
+	System.exit(1);
+    }
 
-  //@Override
-  protected void validate () throws OptionException
-  {
-    //Base implementation does nothing.
-  }
+    /**
+         * Callback function. Gives an array of core files if core files were
+         * detected on the command line.
+         * 
+         * @param coreFiles
+         *                The array of core files passed on the command line.
+         */
+    public void parseCores(Util.CoreExePair[] coreExePairs) {
+	System.err.println("Error: Cores not supported.");
+	printHelp();
+	System.exit(1);
+    }
 
-  public void setHeader (String string)
-  {
-    parser.setHeader(string);
-  }
+    /**
+         * Callback function. Gives a string array represented a parsed command.
+         * 
+         * @param command
+         *                The parsed command.
+         */
+    public void parseCommand(String[] command) {
+	System.err.println("Error: Commands not supported.");
+	printHelp();
+	System.exit(1);
+    }
 
-  public void add (Option option)
-  {
-    parser.add(option);
-  }
+    public String[] parse(String[] args) {
+	String[] result = doParse(args);
+	try {
+	    validate();
+	} catch (OptionException e) {
+	    printHelp();
+	    throw new RuntimeException(e);
+	}
+	return result;
+    }
 
-  public void printHelp ()
-  {
-    parser.printHelp();
-  }
+    private String[] doParse(String[] args) {
+	String[] result = parser.parse(args);
+
+	// XXX: Should parseCommand be called with an empty array here?
+	if (result == null)
+	    return null;
+	if (result.length == 0)
+	    return result;
+
+	// Check if arguments are all pids.
+	try {
+	    ProcId[] pids = new ProcId[result.length];
+	    pids[0] = new ProcId(Integer.parseInt(result[0]));
+
+	    for (int i = 1; i < result.length; i++) {
+		try {
+		    pids[i] = new ProcId(Integer.parseInt(result[i]));
+		} catch (NumberFormatException e) {
+		    throw new RuntimeException(
+			    "Please don't mix pids with core files or executables");
+		}
+	    }
+
+	    parsePids(pids);
+	    return result;
+	} catch (NumberFormatException e) {
+	    // Not a pid, continue on.
+	}
+
+	// Check if arguments are all core file/ exe file pairs..
+	if (isCoreFile(result[0])) {
+	    ArrayList coreExeFiles = new ArrayList();
+
+	    for (int file = 0; file < result.length - 1; file++) {
+		File coreFile;
+		if (isCoreFile(result[file])) {
+		    coreFile = new File(result[file]);
+		    if (isExeFile(result[file + 1])) {
+			coreExeFiles.add(new Util.CoreExePair(coreFile,
+				new File(result[file + 1])));
+			file++;
+		    } else
+			coreExeFiles.add(new Util.CoreExePair(coreFile, null));
+		} else
+		    throw new RuntimeException(
+			    "Please don't mix core files with pids or executables.");
+	    }
+
+	    if (isCoreFile(result[result.length - 1]))
+		coreExeFiles.add(new Util.CoreExePair(new File(
+			result[result.length - 1]), null));
+
+	    Util.CoreExePair[] coreExePairs = new Util.CoreExePair[coreExeFiles
+		    .size()];
+	    System.arraycopy(coreExeFiles.toArray(), 0, coreExePairs, 0,
+		    coreExePairs.length);
+	    parseCores(coreExePairs);
+	    return result;
+	}
+
+	// If not above, then this is an executable command.
+	parseCommand(result);
+	return result;
+    }
+
+    /**
+         * Check if the given file is a coreFile.
+         * 
+         * @param fileName
+         *                A file to check.
+         * @return if fileName is a corefile returns true, otherwise false.
+         */
+    private boolean isCoreFile(String fileName) {
+	Elf elf;
+	try {
+	    elf = new Elf(fileName, ElfCommand.ELF_C_READ);
+	} catch (Exception e) {
+	    return false;
+	}
+
+	boolean ret = elf.getEHeader().type == ElfEHeader.PHEADER_ET_CORE;
+	elf.close();
+	return ret;
+
+    }
+
+    private boolean isExeFile(String fileName) {
+	Elf elf;
+	try {
+	    elf = new Elf(fileName, ElfCommand.ELF_C_READ);
+	} catch (Exception e) {
+	    return false;
+	}
+
+	boolean ret = elf.getEHeader().type == ElfEHeader.PHEADER_ET_EXEC;
+	elf.close();
+	return ret;
+
+    }
+
+    // @Override
+    protected void validate() throws OptionException {
+	// Base implementation does nothing.
+    }
+
+    public void setHeader(String string) {
+	parser.setHeader(string);
+    }
+
+    public void add(Option option) {
+	parser.add(option);
+    }
+
+    public void printHelp() {
+	parser.printHelp();
+    }
 }
-    
