7efd9bfa6d6442907fddb7c23ed45a17a7b93d2d
Andrew Cagney <cagney@redhat.com>
2007-10-01 15:56:50 +0000

    Index: frysk-core/frysk/proc/ChangeLog 2007-10-01  Andrew Cagney  <cagney@redhat.com>
    	* IsaPPC.java (IsaPPC()): Simplify.
    	(PPCRegister): Delete.
    	(PPCFPRegister): Delete.
    	(gprs(), fprs()): Delete.
    	(gpr, fpr): Delete.
    	(nip, msr, orig_r3, ctr, lnk, xer, ccr, mq, trap, dar): Delete.
    	(dsisr, result): Delete.

--- frysk-core/frysk/proc/IsaPPC.java
+++ frysk-core/frysk/proc/IsaPPC.java
@@ -44,94 +44,84 @@ import lib.dwfl.ElfEMachine;
 public class IsaPPC
   extends IsaPowerPC
 {
-  static class PPCRegister
-    extends BankRegister
-  {
-    PPCRegister (String name, int wordOffset)
-    {
-      super (0, wordOffset * 4, 4, name);
+    IsaPPC () {
+	add(new BankRegister(0, 0, 4, "gpr0"));
+	add(new BankRegister(0, 4, 4, "gpr1"));
+	add(new BankRegister(0, 8, 4, "gpr2"));
+	add(new BankRegister(0, 12, 4, "gpr3"));
+	add(new BankRegister(0, 16, 4, "gpr4"));
+	add(new BankRegister(0, 20, 4, "gpr5"));
+	add(new BankRegister(0, 24, 4, "gpr6"));
+	add(new BankRegister(0, 28, 4, "gpr7"));
+	add(new BankRegister(0, 32, 4, "gpr8"));
+	add(new BankRegister(0, 36, 4, "gpr9"));
+	add(new BankRegister(0, 40, 4, "gpr10"));
+	add(new BankRegister(0, 44, 4, "gpr11"));
+	add(new BankRegister(0, 48, 4, "gpr12"));
+	add(new BankRegister(0, 52, 4, "gpr13"));
+	add(new BankRegister(0, 56, 4, "gpr14"));
+	add(new BankRegister(0, 60, 4, "gpr15"));
+	add(new BankRegister(0, 64, 4, "gpr16"));
+	add(new BankRegister(0, 68, 4, "gpr17"));
+	add(new BankRegister(0, 72, 4, "gpr18"));
+	add(new BankRegister(0, 76, 4, "gpr19"));
+	add(new BankRegister(0, 80, 4, "gpr20"));
+	add(new BankRegister(0, 84, 4, "gpr21"));
+	add(new BankRegister(0, 88, 4, "gpr22"));
+	add(new BankRegister(0, 92, 4, "gpr23"));
+	add(new BankRegister(0, 96, 4, "gpr24"));
+	add(new BankRegister(0, 100, 4, "gpr25"));
+	add(new BankRegister(0, 104, 4, "gpr26"));
+	add(new BankRegister(0, 108, 4, "gpr27"));
+	add(new BankRegister(0, 112, 4, "gpr28"));
+	add(new BankRegister(0, 116, 4, "gpr29"));
+	add(new BankRegister(0, 120, 4, "gpr30"));
+	add(new BankRegister(0, 124, 4, "gpr31"));
+	add(new BankRegister(0, 128, 4, "nip"));
+	add(new BankRegister(0, 132, 4, "msr"));
+	add(new BankRegister(0, 136, 4, "orig_r3"));
+	add(new BankRegister(0, 140, 4, "ctr"));
+	add(new BankRegister(0, 144, 4, "lnk"));
+	add(new BankRegister(0, 148, 4, "xer"));
+	add(new BankRegister(0, 152, 4, "ccr"));
+	add(new BankRegister(0, 156, 4, "mq"));
+	add(new BankRegister(0, 160, 4, "trap"));
+	add(new BankRegister(0, 164, 4, "dar"));
+	add(new BankRegister(0, 168, 4, "dsisr"));
+	add(new BankRegister(0, 172, 4, "result"));
+	add(new BankRegister(0, 192, 8, "fpr0"));
+	add(new BankRegister(0, 200, 8, "fpr1"));
+	add(new BankRegister(0, 208, 8, "fpr2"));
+	add(new BankRegister(0, 216, 8, "fpr3"));
+	add(new BankRegister(0, 224, 8, "fpr4"));
+	add(new BankRegister(0, 232, 8, "fpr5"));
+	add(new BankRegister(0, 240, 8, "fpr6"));
+	add(new BankRegister(0, 248, 8, "fpr7"));
+	add(new BankRegister(0, 256, 8, "fpr8"));
+	add(new BankRegister(0, 264, 8, "fpr9"));
+	add(new BankRegister(0, 272, 8, "fpr10"));
+	add(new BankRegister(0, 280, 8, "fpr11"));
+	add(new BankRegister(0, 288, 8, "fpr12"));
+	add(new BankRegister(0, 296, 8, "fpr13"));
+	add(new BankRegister(0, 304, 8, "fpr14"));
+	add(new BankRegister(0, 312, 8, "fpr15"));
+	add(new BankRegister(0, 320, 8, "fpr16"));
+	add(new BankRegister(0, 328, 8, "fpr17"));
+	add(new BankRegister(0, 336, 8, "fpr18"));
+	add(new BankRegister(0, 344, 8, "fpr19"));
+	add(new BankRegister(0, 352, 8, "fpr20"));
+	add(new BankRegister(0, 360, 8, "fpr21"));
+	add(new BankRegister(0, 368, 8, "fpr22"));
+	add(new BankRegister(0, 376, 8, "fpr23"));
+	add(new BankRegister(0, 384, 8, "fpr24"));
+	add(new BankRegister(0, 392, 8, "fpr25"));
+	add(new BankRegister(0, 400, 8, "fpr26"));
+	add(new BankRegister(0, 408, 8, "fpr27"));
+	add(new BankRegister(0, 416, 8, "fpr28"));
+	add(new BankRegister(0, 424, 8, "fpr29"));
+	add(new BankRegister(0, 432, 8, "fpr30"));
+	add(new BankRegister(0, 440, 8, "fpr31"));
     }
-    PPCRegister (String name, int offset, int wordSize)
-    {
-      super (0, offset, wordSize, name);
-    }
-  }
-  static class PPCFPRegister
-    extends PPCRegister
-  {
-    // Since on ppc, the word size is still 32-bit, even if FP is 
-    // 64-bit in width.
-    PPCFPRegister (String name, int wordOffset)
-    {
-      super (name, wordOffset * 4, 8);
-    }
-  }
-  
-  private static PPCRegister[] gprs ()
-  {
-    PPCRegister[] gprs = new PPCRegister[32];
-    for (int i = 0; i < gprs.length; i++) 
-      {
-	gprs[i] = new PPCRegister ("gpr" + i, i);
-      }
-    return gprs;
-  }
-
-  private static PPCRegister[] fprs ()
-  {
-    PPCRegister[] fprs = new PPCFPRegister[32];
-    for (int i = 0; i < fprs.length; i++) 
-      {
-	// Please reference /usr/include/asm/ptrace.h, in which PT_FPR8
-	// is 48, and every FP occupies 2 slots.
-	fprs[i] = new PPCFPRegister ("fpr" + i, 48 + 2 * i);
-      }
-    return fprs;
-  }
-
-  private static final PPCRegister[] gpr = gprs();
- 
-  private static final PPCRegister nip = new PPCRegister("nip", 32);
-  private static final PPCRegister msr = new PPCRegister("msr", 33);
-  private static final PPCRegister orig_r3 = new PPCRegister("orig_r3", 34);
-  private static final PPCRegister ctr = new PPCRegister("ctr", 35);
-  private static final PPCRegister lnk = new PPCRegister("lnk", 36);
-  private static final PPCRegister xer = new PPCRegister("xer", 37);
-  private static final PPCRegister ccr = new PPCRegister("ccr", 38);
-  private static final PPCRegister mq = new PPCRegister("mq", 39);
-  private static final PPCRegister trap = new PPCRegister("trap", 40);
-  private static final PPCRegister dar = new PPCRegister("dar", 41);
-  private static final PPCRegister dsisr = new PPCRegister("dsisr", 42);
-  private static final PPCRegister result = new PPCRegister("result", 43);
-
-  private static final PPCRegister[] fpr = fprs();
-
-  IsaPPC ()
-  {
-    for (int i = 0; i < gpr.length; i++) 
-      {
-	registerMap.put (gpr[i].getName(), gpr[i]);
-      }
-
-    registerMap.put(nip.getName(), nip);
-    registerMap.put(msr.getName(), msr);
-    registerMap.put(orig_r3.getName(), orig_r3);
-    registerMap.put(ctr.getName(), ctr);
-    registerMap.put(lnk.getName(), lnk);
-    registerMap.put(xer.getName(), xer);
-    registerMap.put(ccr.getName(), ccr);
-    registerMap.put(mq.getName(), mq);
-    registerMap.put(trap.getName(), trap);
-    registerMap.put(dar.getName(), dar);
-    registerMap.put(dsisr.getName(), dsisr);
-    registerMap.put(result.getName(), result);
-
-    for (int i = 0; i < fpr.length; i++)
-      {
-	// FP Register Map needed here?
-	registerMap.put (fpr[i].getName(), fpr[i]);
-      }
-  }
     
   public int getWordSize ()
   {
