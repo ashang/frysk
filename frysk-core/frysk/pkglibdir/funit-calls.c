616c9bbb472fb0b0d23e4ae233c2f19926fe8d20
Petr Machata <pmachata@redhat.com>
2007-11-02 16:51:55 +0100

    New test for ltrace
    Testing whether arguments recorded at function call match reality, and that return values are as expected.

---
+++ frysk-core/frysk/pkglibdir/funit-calls.c
@@ -0,0 +1,57 @@
+// This file is part of the program FRYSK.
+//
+// Copyright 2005, 2006, 2007 Red Hat Inc.
+//
+// FRYSK is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// FRYSK is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with FRYSK; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+// 
+// In addition, as a special exception, Red Hat, Inc. gives You the
+// additional right to link the code of FRYSK with code not covered
+// under the GNU General Public License ("Non-GPL Code") and to
+// distribute linked combinations including the two, subject to the
+// limitations in this paragraph. Non-GPL Code permitted under this
+// exception must only link to the code of FRYSK through those well
+// defined interfaces identified in the file named EXCEPTION found in
+// the source code files (the "Approved Interfaces"). The files of
+// Non-GPL Code may instantiate templates or use macros or inline
+// functions from the Approved Interfaces without causing the
+// resulting work to be covered by the GNU General Public
+// License. Only Red Hat, Inc. may make changes or additions to the
+// list of Approved Interfaces. You must obey the GNU General Public
+// License in all respects for all of the FRYSK code and other code
+// used in conjunction with FRYSK except the Non-GPL Code covered by
+// this exception. If you modify this file, you may extend this
+// exception to your version of the file, but you are not obligated to
+// do so. If you do not wish to provide this exception without
+// modification, you must delete this exception statement from your
+// version and license this file solely under the GPL without
+// exception.
+
+/* A little program that does some function calls.  For testing
+   ltrace.  */
+
+#include <stdlib.h>
+
+int trace_me_1(int a1, int a2, int a3, int a4, int a5, int a6) {
+  return a1 + a2 + a3 + a4 + a5 + a6;
+}
+
+long trace_me_2(long a1, long a2, long a3, long a4, long a5, long a6) {
+  return a1 + a2 + a3 + a4 + a5 + a6;
+}
+
+int main() {
+  trace_me_1(3, 5, 7, 11, 13, 17);
+  trace_me_2(3, 5, 7, 11, 13, 17);
+  exit(0);
+}
