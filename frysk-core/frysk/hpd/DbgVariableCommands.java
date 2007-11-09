6d8e45c3d597cb380215da40c2a6b02702ee4b9d
Andrew Cagney <cagney@redhat.com>
2007-11-09 15:48:59 -0500

    Refactor unset and set; test.
    frysk-core/frysk/hpd/ChangeLog
    2007-11-09  Andrew Cagney  <cagney@redhat.com>
    
    	* DbgVariables.java (complete(String,int,List)): New.
    	* TestDbgVariables.java: New.
    	* DbgVariableCommands: New.
    	* SetCommand.java: Delete.
    	* UnsetCommand.java: Delete.
    	* TopLevelCommand.java: Update.
    	* CLI.java (dbgvars): Make final; update.

--- frysk-core/frysk/hpd/DbgVariableCommands.java
+++ frysk-core/frysk/hpd/DbgVariableCommands.java
@@ -101,3 +101,107 @@ class SetCommand extends ParameterizedCommand {
 	return -1;
     }
 }
+
+// This file is part of the program FRYSK.
+//
+// Copyright 2005, 2006, 2007 Red Hat Inc.
+//
+// FRYSK is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// FRYSK is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with FRYSK; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+// 
+// In addition, as a special exception, Red Hat, Inc. gives You the
+// additional right to link the code of FRYSK with code not covered
+// under the GNU General Public License ("Non-GPL Code") and to
+// distribute linked combinations including the two, subject to the
+// limitations in this paragraph. Non-GPL Code permitted under this
+// exception must only link to the code of FRYSK through those well
+// defined interfaces identified in the file named EXCEPTION found in
+// the source code files (the "Approved Interfaces"). The files of
+// Non-GPL Code may instantiate templates or use macros or inline
+// functions from the Approved Interfaces without causing the
+// resulting work to be covered by the GNU General Public
+// License. Only Red Hat, Inc. may make changes or additions to the
+// list of Approved Interfaces. You must obey the GNU General Public
+// License in all respects for all of the FRYSK code and other code
+// used in conjunction with FRYSK except the Non-GPL Code covered by
+// this exception. If you modify this file, you may extend this
+// exception to your version of the file, but you are not obligated to
+// do so. If you do not wish to provide this exception without
+// modification, you must delete this exception statement from your
+// version and license this file solely under the GPL without
+// exception.
+
+package frysk.hpd;
+
+import java.util.List;
+
+class UnsetCommand extends ParameterizedCommand {
+    private final DbgVariables dbgvars;
+
+    UnsetCommand(DbgVariables dbgvars) {
+	super("unset", "Revert variable value to default.",
+	      "unset [ <debugger-var> | -all ]",
+	      ("The unset command reverses the effects of any previous"
+	       + " set operations, restoring the debugger state"
+	       + " variable(s) to their default settings.  When the"
+	       + " argument -all is specified, the command affects all"
+	       + " debugger state variables, restoring them to the"
+	       + " original settings that were in effect when the"
+	       + " debugging session began.  When just a single"
+	       + " argument is included, only that variable is affected."));
+	this.dbgvars = dbgvars;
+	add(new CommandOption("all", "Revert all variables") {
+		void parse(String arg, Object option) {
+		    ((Options)option).all = true;
+		}
+	    });
+    }
+
+    private class Options {
+	boolean all = false;
+    }
+    Object options() {
+	return new Options();
+    }
+
+    int complete(CLI cli, PTSet ptset, String incomplete, int base,
+		 List completions) {
+	return -1;
+    }
+
+    public void interpret(CLI cli, Input input, Object o) {
+	Options options = (Options)o;
+	if (options.all) {
+	    if (input.size() != 0)
+		throw new InvalidCommandException
+		    ("Too many arguments for -all");
+	    dbgvars.unsetAll();
+	    cli.outWriter.println("All options reset");
+	    return;
+	}
+
+	switch (input.size()) {
+	case 0:
+	    throw new InvalidCommandException("Missing argument");
+	case 1:
+	    String temp = input.parameter(0);
+	    if (!dbgvars.variableIsValid(temp))
+		throw new InvalidCommandException
+		    ("\"" + temp + "\" is not a valid debugger variable");
+	    dbgvars.unsetVariable(temp);
+	    break;
+	default:
+	    throw new InvalidCommandException("Too many arguments");
+	}
+    }
+}
