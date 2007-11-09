8b867fb62647409597fd921ec3e0888e53976190
Andrew Cagney <cagney@redhat.com>
2007-11-09 15:50:11 -0500

    Second half of refactor Set/Unset.
    frysk-core/frysk/hpd/ChangeLog
    2007-11-09  Andrew Cagney  <cagney@redhat.com>
    
    	* DbgVariables.java (complete(String,int,List)): New.
    	* TestDbgVariables.java: New.
    	* DbgVariableCommands: New.
    	* SetCommand.java: Delete.
    	* UnsetCommand.java: Delete.
    	* TopLevelCommand.java: Update.
    	* CLI.java (dbgvars): Make final; update.

--- frysk-core/frysk/hpd/DbgVariableCommands.java
+++ frysk-core/frysk/hpd/DbgVariableCommands.java
@@ -41,167 +41,122 @@ package frysk.hpd;
 
 import java.util.List;
 
-class SetCommand extends ParameterizedCommand {
+abstract class DbgVariableCommands extends ParameterizedCommand {
 
-    private final DbgVariables dbgvars;
-
-    SetCommand(DbgVariables dbgvars) {
-	super("set", "Change or view a debugger variable.",
-	      "set debugger-var = value\nset [debugger-var]",
-	      ("The set command supports the viewing of debugger state"
-	       + " variables and the assignment of new values to them.  When"
-	       + " no arguments are specified, the names and current values"
-	       + " for all debugger state variables are displayed.  When"
-	       + " just a single argument is included, the debugger echoes"
-	       + " the variable name and displays its current value.  The"
-	       + " second argument defines the value that should replace"
-	       + " any previous value for that variable.  It must be"
-	       + " enclosed in quotes if it contains multiple words."));
-	this.dbgvars = dbgvars;
-    }
-
-    public void interpret(CLI cli, Input input, Object options) {
-	switch (input.size()) {
-	default:
-	    throw new InvalidCommandException("wrong number of parameters");
-	case 3:
-	    if (!input.parameter(1).equals("="))
-		throw new InvalidCommandException("missing \"=\"");
-	    String variable = input.parameter(0);
-	    if (!dbgvars.variableIsValid(variable))
-		throw new InvalidCommandException("Debugger variable \""
-						  + variable
-						  + "\" is invalid");
-	    String value = input.parameter(2);
-	    if (!dbgvars.valueIsValid(variable, value))
-		throw new InvalidCommandException("Variable value \""
-						  + value
-						  + "\" is invalid");
-	    dbgvars.setVariable(variable, value);
-	    break;
-	case 1:
-	    String var = input.parameter(0);
-	    if (!dbgvars.variableIsValid(var))
-		throw new InvalidCommandException("Variable \""
-						  + var
-						  + "\" is invalid");
-	    cli.outWriter.print(var);
-	    cli.outWriter.print(" = ");
-	    cli.outWriter.print(dbgvars.getValue(var));
-	    cli.outWriter.println();
-	    break;
-	case 0:
-	    cli.outWriter.println(dbgvars.toString());
-	    break;
-	}
+    DbgVariableCommands(String name, String description,
+			String syntax, String full) {
+	super(name, description, syntax, full);
     }
 
     int complete(CLI cli, PTSet ptset, String incomplete, int base,
 		 List completions) {
-	return -1;
-    }
-}
-
-// This file is part of the program FRYSK.
-//
-// Copyright 2005, 2006, 2007 Red Hat Inc.
-//
-// FRYSK is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation; version 2 of the License.
-//
-// FRYSK is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with FRYSK; if not, write to the Free Software Foundation,
-// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
-// 
-// In addition, as a special exception, Red Hat, Inc. gives You the
-// additional right to link the code of FRYSK with code not covered
-// under the GNU General Public License ("Non-GPL Code") and to
-// distribute linked combinations including the two, subject to the
-// limitations in this paragraph. Non-GPL Code permitted under this
-// exception must only link to the code of FRYSK through those well
-// defined interfaces identified in the file named EXCEPTION found in
-// the source code files (the "Approved Interfaces"). The files of
-// Non-GPL Code may instantiate templates or use macros or inline
-// functions from the Approved Interfaces without causing the
-// resulting work to be covered by the GNU General Public
-// License. Only Red Hat, Inc. may make changes or additions to the
-// list of Approved Interfaces. You must obey the GNU General Public
-// License in all respects for all of the FRYSK code and other code
-// used in conjunction with FRYSK except the Non-GPL Code covered by
-// this exception. If you modify this file, you may extend this
-// exception to your version of the file, but you are not obligated to
-// do so. If you do not wish to provide this exception without
-// modification, you must delete this exception statement from your
-// version and license this file solely under the GPL without
-// exception.
-
-package frysk.hpd;
-
-import java.util.List;
-
-class UnsetCommand extends ParameterizedCommand {
-    private final DbgVariables dbgvars;
-
-    UnsetCommand(DbgVariables dbgvars) {
-	super("unset", "Revert variable value to default.",
-	      "unset [ <debugger-var> | -all ]",
-	      ("The unset command reverses the effects of any previous"
-	       + " set operations, restoring the debugger state"
-	       + " variable(s) to their default settings.  When the"
-	       + " argument -all is specified, the command affects all"
-	       + " debugger state variables, restoring them to the"
-	       + " original settings that were in effect when the"
-	       + " debugging session began.  When just a single"
-	       + " argument is included, only that variable is affected."));
-	this.dbgvars = dbgvars;
-	add(new CommandOption("all", "Revert all variables") {
-		void parse(String arg, Object option) {
-		    ((Options)option).all = true;
-		}
-	    });
+	return cli.dbgvars.complete(incomplete, base, completions);
     }
 
-    private class Options {
-	boolean all = false;
-    }
-    Object options() {
-	return new Options();
-    }
-
-    int complete(CLI cli, PTSet ptset, String incomplete, int base,
-		 List completions) {
-	return -1;
-    }
-
-    public void interpret(CLI cli, Input input, Object o) {
-	Options options = (Options)o;
-	if (options.all) {
-	    if (input.size() != 0)
+    static class Set extends DbgVariableCommands {
+	Set() {
+	    super("set", "Change or view a debugger variable.",
+		  "set debugger-var = value\nset [debugger-var]",
+		  ("The set command supports the viewing of debugger state"
+		   + " variables and the assignment of new values to them.  When"
+		   + " no arguments are specified, the names and current values"
+		   + " for all debugger state variables are displayed.  When"
+		   + " just a single argument is included, the debugger echoes"
+		   + " the variable name and displays its current value.  The"
+		   + " second argument defines the value that should replace"
+		   + " any previous value for that variable.  It must be"
+		   + " enclosed in quotes if it contains multiple words."));
+	}
+	public void interpret(CLI cli, Input input, Object options) {
+	    switch (input.size()) {
+	    default:
 		throw new InvalidCommandException
-		    ("Too many arguments for -all");
-	    dbgvars.unsetAll();
-	    cli.outWriter.println("All options reset");
-	    return;
+		    ("wrong number of parameters");
+	    case 0:
+		cli.outWriter.println(cli.dbgvars.toString());
+		break;
+	    case 1:
+		String var = input.parameter(0);
+		if (!cli.dbgvars.variableIsValid(var))
+		    throw new InvalidCommandException("Debug variable \""
+						      + var
+						      + "\" is invalid");
+		cli.outWriter.print(var);
+		cli.outWriter.print(" = ");
+		cli.outWriter.print(cli.dbgvars.getValue(var));
+		cli.outWriter.println();
+		break;
+	    case 2:
+	    case 3:
+		if (input.size() == 3 && !input.parameter(1).equals("="))
+		    throw new InvalidCommandException("missing \"=\"");
+		String variable = input.parameter(0);
+		if (!cli.dbgvars.variableIsValid(variable))
+		    throw new InvalidCommandException("Debugger variable \""
+						      + variable
+						      + "\" is invalid");
+		String value = input.parameter(input.size() - 1);
+		if (!cli.dbgvars.valueIsValid(variable, value))
+		    throw new InvalidCommandException("Debug variable value \""
+						      + value
+						      + "\" is invalid");
+		cli.dbgvars.setVariable(variable, value);
+		break;
+	    }
+	}
+    }
+
+    static class Unset extends DbgVariableCommands {
+	Unset() {
+	    super("unset", "Revert variable value to default.",
+		  "unset [ <debugger-var> | -all ]",
+		  ("The unset command reverses the effects of any previous"
+		   + " set operations, restoring the debugger state"
+		   + " variable(s) to their default settings.  When the"
+		   + " argument -all is specified, the command affects all"
+		   + " debugger state variables, restoring them to the"
+		   + " original settings that were in effect when the"
+		   + " debugging session began.  When just a single"
+		   + " argument is included, only that variable is"
+		   + " affected."));
+	    add(new CommandOption("all", "Revert all variables") {
+		    void parse(String arg, Object option) {
+			((Options)option).all = true;
+		    }
+		});
 	}
 
-	switch (input.size()) {
-	case 0:
-	    throw new InvalidCommandException("Missing argument");
-	case 1:
-	    String temp = input.parameter(0);
-	    if (!dbgvars.variableIsValid(temp))
-		throw new InvalidCommandException
-		    ("\"" + temp + "\" is not a valid debugger variable");
-	    dbgvars.unsetVariable(temp);
-	    break;
-	default:
-	    throw new InvalidCommandException("Too many arguments");
+	private class Options {
+	    boolean all = false;
+	}
+	Object options() {
+	    return new Options();
+	}
+
+	public void interpret(CLI cli, Input input, Object o) {
+	    Options options = (Options)o;
+	    if (options.all) {
+		if (input.size() != 0)
+		    throw new InvalidCommandException
+			("Too many arguments for -all");
+		cli.dbgvars.unsetAll();
+		cli.outWriter.println("All debug variables reset");
+		return;
+	    }
+
+	    switch (input.size()) {
+	    case 0:
+		throw new InvalidCommandException("Missing argument");
+	    case 1:
+		String temp = input.parameter(0);
+		if (!cli.dbgvars.variableIsValid(temp))
+		    throw new InvalidCommandException
+			("\"" + temp + "\" is not a valid debugger variable");
+		cli.dbgvars.unsetVariable(temp);
+		break;
+	    default:
+		throw new InvalidCommandException("Too many arguments");
+	    }
 	}
     }
 }
--- frysk-core/frysk/hpd/DbgVariables.java
+++ frysk-core/frysk/hpd/DbgVariables.java
@@ -1,6 +1,6 @@
 // This file is part of the program FRYSK.
 //
-// Copyright 2006, Red Hat Inc.
+// Copyright 2006, 2007, Red Hat Inc.
 //
 // FRYSK is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
@@ -36,160 +36,149 @@
 // modification, you must delete this exception statement from your
 // version and license this file solely under the GPL without
 // exception.
+
 package frysk.hpd;
 
-import java.util.Hashtable;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.LinkedList;
 import java.util.Arrays;
 import java.lang.Integer;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Debugger variable accessible through the "set" command.
  */
-class DbgVariables
-{
-	public static int VARTYPE_INT = 0;
-	public static int VARTYPE_STRING = 1;
-	public static int VARTYPE_CUSTOM = 2;
+class DbgVariables {
+    public static int VARTYPE_INT = 0;
+    public static int VARTYPE_STRING = 1;
+    public static int VARTYPE_CUSTOM = 2;
 
-	Hashtable vars;
+    static private class Value {
+	private int type;
+	private Object value;
+	private Object deflt;
+	private LinkedList validVals;
 
-	class Value
-	{
-		private int type;
-		private Object value;
-		private Object deflt;
-		private LinkedList validVals;
-
-		public Value(int type, Object deflt)
-		{
-			this.type = type;
-			this.value = deflt;
-			this.deflt = deflt;
-		}
-
-		public Value(int type, Object deflt, String[] validVals)
-		{
-			this.type = type;
-			this.value = deflt;
-			this.deflt = deflt;
-			this.validVals = new LinkedList(Arrays.asList(validVals));
-		}
-
-		public int getType()
-		{
-			return type;
-		}
-
-		public Object getValue()
-		{
-			return value;
-		}
-
-		public Object getDefault()
-		{
-			return deflt;
-		}
-
-		public void setValue(Object val)
-		{
-			value = val;
-		}
-
-		public boolean valueValid(String val)
-		{
-			return validVals.contains(val);
-		}
-
-		public String toString()
-		{
-			return value.toString();
-		}
+	public Value(int type, Object deflt) {
+	    this.type = type;
+	    this.value = deflt;
+	    this.deflt = deflt;
 	}
 
-	public DbgVariables()
-	{
-		vars = new Hashtable();
-
-		vars.put("MODE", new Value(VARTYPE_CUSTOM, "MULTILEVEL", new String[] {"THREADS", "PROCESSES", "MULTILEVEL"}));
-		vars.put("START_MODEL", new Value(VARTYPE_CUSTOM, "ANY", new String[] {"ANY", "ALL"}));
-		vars.put("STOP_MODEL", new Value(VARTYPE_CUSTOM, "ANY", new String[] {"ANY", "ALL"}));
-		vars.put("EVENT_INTERRUPT", new Value(VARTYPE_CUSTOM, "ON", new String[] {"ON", "OFF"}));
-		vars.put("VERBOSE", new Value(VARTYPE_CUSTOM, "WARN", new String[] {"WARN", "ERR", "ALL"}));
-		vars.put("ERROR_CHECKS", new Value(VARTYPE_CUSTOM, "NORMAL", new String[] {"NORMAL", "MIN", "MAX"}));
-		vars.put("MAX_PROMPT", new Value(VARTYPE_INT, new Integer(40)));
-		vars.put("MAX_HISTORY", new Value(VARTYPE_INT, new Integer(20)));
-		vars.put("MAX_LEVELS", new Value(VARTYPE_INT, new Integer(20)));
-		vars.put("MAX_LIST", new Value(VARTYPE_INT, new Integer(20)));
-		vars.put("PROMPT", new Value(VARTYPE_STRING, "(frysk) "));
-		vars.put("SOURCE_PATH", new Value(VARTYPE_STRING, ""));
-		vars.put("EXECUTABLE_PATH", new Value(VARTYPE_STRING, "./:" + System.getenv("PATH")));
+	public Value(int type, Object deflt, String[] validVals) {
+	    this.type = type;
+	    this.value = deflt;
+	    this.deflt = deflt;
+	    this.validVals = new LinkedList(Arrays.asList(validVals));
 	}
 
-	public void setVariable(String var, String value)
-	{
-		Value tempval = (Value)vars.get(var);
-
-		if (var.equals("MAX_PROMPT") || var.equals("MAX_HISTORY") ||
-				var.equals("MAX_LEVELS") || var.equals("MAX_LIST"))
-			tempval.setValue(Integer.getInteger(value));
-		else 
-			tempval.setValue(value);
+	public int getType() {
+	    return type;
 	}
 
-	public void unsetVariable(String var)
-	{
-		Value tempval = (Value)vars.get(var);
-		tempval.setValue(tempval.getDefault());
+	public Object getValue() {
+	    return value;
 	}
 
-	public void unsetAll()
-	{
-		Value tempval;
-
-		for (Iterator iter = vars.values().iterator(); iter.hasNext();)
-		{
-			tempval = (Value)iter.next();
-			tempval.setValue(tempval.getDefault());
-		}
+	public Object getDefault() {
+	    return deflt;
 	}
 
-	public boolean variableIsValid(String var)
-	{
-		return vars.containsKey(var);
+	public void setValue(Object val) {
+	    value = val;
 	}
 
-	public boolean valueIsValid(String var, String value)
-	{
-		boolean result = true;
-		Value tempval = (Value) vars.get(var);
+	public boolean valueValid(String val) {
+	    return validVals.contains(val);
+	}
+
+	public String toString() {
+	    return value.toString();
+	}
+    }
+
+    private final SortedMap vars = new TreeMap();
+
+    public DbgVariables() {
+	vars.put("MODE", new Value(VARTYPE_CUSTOM, "MULTILEVEL", new String[] {"THREADS", "PROCESSES", "MULTILEVEL"}));
+	vars.put("START_MODEL", new Value(VARTYPE_CUSTOM, "ANY", new String[] {"ANY", "ALL"}));
+	vars.put("STOP_MODEL", new Value(VARTYPE_CUSTOM, "ANY", new String[] {"ANY", "ALL"}));
+	vars.put("EVENT_INTERRUPT", new Value(VARTYPE_CUSTOM, "ON", new String[] {"ON", "OFF"}));
+	vars.put("VERBOSE", new Value(VARTYPE_CUSTOM, "WARN", new String[] {"WARN", "ERR", "ALL"}));
+	vars.put("ERROR_CHECKS", new Value(VARTYPE_CUSTOM, "NORMAL", new String[] {"NORMAL", "MIN", "MAX"}));
+	vars.put("MAX_PROMPT", new Value(VARTYPE_INT, new Integer(40)));
+	vars.put("MAX_HISTORY", new Value(VARTYPE_INT, new Integer(20)));
+	vars.put("MAX_LEVELS", new Value(VARTYPE_INT, new Integer(20)));
+	vars.put("MAX_LIST", new Value(VARTYPE_INT, new Integer(20)));
+	vars.put("PROMPT", new Value(VARTYPE_STRING, "(frysk) "));
+	vars.put("SOURCE_PATH", new Value(VARTYPE_STRING, ""));
+	vars.put("EXECUTABLE_PATH", new Value(VARTYPE_STRING, "./:" + System.getenv("PATH")));
+    }
+
+    public void setVariable(String var, String value) {
+	Value tempval = (Value)vars.get(var);
+	if (var.equals("MAX_PROMPT") || var.equals("MAX_HISTORY") ||
+	    var.equals("MAX_LEVELS") || var.equals("MAX_LIST"))
+	    tempval.setValue(Integer.getInteger(value));
+	else 
+	    tempval.setValue(value);
+    }
+
+    public void unsetVariable(String var) {
+	Value tempval = (Value)vars.get(var);
+	tempval.setValue(tempval.getDefault());
+    }
+
+    public void unsetAll() {
+	Value tempval;
+	for (Iterator iter = vars.values().iterator(); iter.hasNext();) {
+	    tempval = (Value)iter.next();
+	    tempval.setValue(tempval.getDefault());
+	}
+    }
+
+    public boolean variableIsValid(String var) {
+	return vars.containsKey(var);
+    }
+
+    public boolean valueIsValid(String var, String value) {
+	boolean result = true;
+	Value tempval = (Value) vars.get(var);
 		
-		if (tempval.getType() == VARTYPE_INT && Integer.getInteger(value) == null)
-			result = false;
-		else if (tempval.getType() == VARTYPE_CUSTOM && !tempval.valueValid(value))
-			result = false;
+	if (tempval.getType() == VARTYPE_INT
+	    && Integer.getInteger(value) == null)
+	    result = false;
+	else if (tempval.getType() == VARTYPE_CUSTOM
+		 && !tempval.valueValid(value))
+	    result = false;
+	return result;
+    }
 
-		return result;
-	}
+    public int getIntValue(String var) {
+	return ((Integer)vars.get(var)).intValue();
+    }
 
-	public int getIntValue(String var)
-	{
-		return ((Integer)vars.get(var)).intValue();
-	}
+    public String getStringValue(String var) {
+	return (String)vars.get(var);
+    }
 
-	public String getStringValue(String var)
-	{
-		return (String)vars.get(var);
-	}
+    public Object getValue(String var) {
+	return ((Value)vars.get(var)).getValue();
+    }
 
-	public Object getValue(String var)
-	{
-		return ((Value)vars.get(var)).getValue();
-	}
+    public String toString() {
+	return vars.toString();
+    }
 
-	public String toString()
-	{
-		return vars.toString();
+    int complete(String incomplete, int base, List candidates) {
+	for (Iterator i = vars.keySet().iterator(); i.hasNext(); ) {
+	    String key = (String)i.next();
+	    if (key.startsWith(incomplete))
+		candidates.add(key);
 	}
+	CompletionFactory.padSingleCandidate(candidates);
+	return 0;
+    }
 }
