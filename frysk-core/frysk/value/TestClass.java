d4254717bf0c43cbeeda8a57794d5882a7eca924
Nurdin Premji <nurdin@localhost.localdomain>
2008-01-07 18:57:07 -0500

    Updated frysk.value.TestClass

--- frysk-core/frysk/value/TestClass.java
+++ frysk-core/frysk/value/TestClass.java
@@ -3,15 +3,26 @@ package frysk.value;
 import frysk.debuginfo.DebugInfoFrame;
 import frysk.debuginfo.DebugInfoStackFactory;
 import frysk.debuginfo.ObjectDeclarationSearchEngine;
-import frysk.isa.ISA;
 import frysk.junit.TestCase;
 import frysk.proc.Task;
 import frysk.testbed.DaemonBlockedAtSignal;
 
 public class TestClass extends TestCase {
 
-    private CompositeType getType(Variable variable, ISA isa) {
-	Type type = variable.getType(isa);
+    private CompositeType getType(String program, String variableName) {
+	Task task = (new DaemonBlockedAtSignal(program)).getMainTask();
+	DebugInfoFrame frame = DebugInfoStackFactory
+		.createDebugInfoStackTrace(task);
+
+	ObjectDeclarationSearchEngine objectDeclarationSearchEngine = new ObjectDeclarationSearchEngine(
+		frame);
+
+	Variable variable = (Variable) objectDeclarationSearchEngine
+		.getVariable(variableName);
+
+	assertNotNull("Variable found", variable);
+
+	Type type = variable.getType(frame.getTask().getISA());
 	CompositeType compType = null;
 
 	try {
@@ -24,109 +35,46 @@ public class TestClass extends TestCase {
     }
 
     public void testSimpleClass() {
-	String fileName = "funit-simple-class";
-	Task task = (new DaemonBlockedAtSignal(fileName)).getMainTask();
-	DebugInfoFrame frame = DebugInfoStackFactory
-		.createDebugInfoStackTrace(task);
-	ObjectDeclarationSearchEngine objectDeclarationSearchEngine = new ObjectDeclarationSearchEngine(
-		frame);
-
-	Variable variable = (Variable) objectDeclarationSearchEngine
-		.getVariable("simple");
-
-	assertNotNull("Variable found", variable);
-
-	CompositeType type = getType(variable, frame.getTask().getISA());
+	CompositeType type = getType("funit-simple-class", "simple");
 
 	assertEquals("Variable is a class", type.getPrefix(), "class");
     }
 
     public void testComplexClass() {
-	String fileName = "funit-complex-class";
-	Task task = (new DaemonBlockedAtSignal(fileName)).getMainTask();
-	DebugInfoFrame frame = DebugInfoStackFactory
-		.createDebugInfoStackTrace(task);
-	ObjectDeclarationSearchEngine objectDeclarationSearchEngine = new ObjectDeclarationSearchEngine(
-		frame);
+	CompositeType type = getType("funit-complex-class", "complex");
 
-	Variable variable = (Variable) objectDeclarationSearchEngine
-		.getVariable("complex");
-
-	assertNotNull("Variable found", variable);
-
-	CompositeType type = getType(variable, frame.getTask().getISA());
-
-	if (unresolvedCompilerNoSupportForAT_CLASS())
-	    return;
-
-	assertEquals("Variable is a class", type.getPrefix(), "class");
+	assertEquals("Variable is a class", "class", type.getPrefix());
     }
 
     public void testSimpleStruct() {
-	String fileName = "funit-simple-struct";
-	Task task = (new DaemonBlockedAtSignal(fileName)).getMainTask();
-	DebugInfoFrame frame = DebugInfoStackFactory
-		.createDebugInfoStackTrace(task);
-	ObjectDeclarationSearchEngine objectDeclarationSearchEngine = new ObjectDeclarationSearchEngine(
-		frame);
+	CompositeType type = getType("funit-simple-struct", "simple");
 
-	Variable variable = (Variable) objectDeclarationSearchEngine
-		.getVariable("simple");
-
-	assertNotNull("Variable found", variable);
-
-	CompositeType type = getType(variable, frame.getTask().getISA());
-
-	assertEquals("Variable is a struct", type.getPrefix(), "struct");
-
+	assertEquals("Variable is a struct", "struct", type.getPrefix());
     }
 
     public void testComplexStruct() {
-	String fileName = "funit-complex-struct";
-	Task task = (new DaemonBlockedAtSignal(fileName)).getMainTask();
-	DebugInfoFrame frame = DebugInfoStackFactory
-		.createDebugInfoStackTrace(task);
-	ObjectDeclarationSearchEngine objectDeclarationSearchEngine = new ObjectDeclarationSearchEngine(
-		frame);
-
-	Variable variable = (Variable) objectDeclarationSearchEngine
-		.getVariable("complex");
-
-	assertNotNull("Variable found", variable);
-
-	CompositeType type = getType(variable, frame.getTask().getISA());
+	CompositeType type = getType("funit-complex-struct", "complex");
 
 	// Check the type of the variable, new compiler says struct, old
 	// compiler says class.
 	if (unresolvedCompilerNoSupportForAT_CLASS())
-	    assertEquals("Variable is considered a class", type.getPrefix(),
-		    "class");
+	    assertEquals("Variable is considered a class", "class", type
+		    .getPrefix());
 	else
-	    assertEquals("Variable is a struct", type.getPrefix(), "struct");
+	    assertEquals("Variable is a struct", "struct", type.getPrefix());
 
     }
 
     public void testInheritedStruct() {
-	String fileName = "funit-inherited-struct";
-	Task task = (new DaemonBlockedAtSignal(fileName)).getMainTask();
-	DebugInfoFrame frame = DebugInfoStackFactory
-		.createDebugInfoStackTrace(task);
-	ObjectDeclarationSearchEngine objectDeclarationSearchEngine = new ObjectDeclarationSearchEngine(
-		frame);
-
-	Variable variable = (Variable) objectDeclarationSearchEngine
-		.getVariable("der");
-
-	assertNotNull("Variable found", variable);
-
-	CompositeType type = getType(variable, frame.getTask().getISA());
+	CompositeType type = getType("funit-inherited-struct", "der");
 
 	// Check the type of the variable, new compiler says struct, old
 	// compiler says class.
 	if (unresolvedCompilerNoSupportForAT_CLASS())
-	    assertEquals("Variable is considered a class", type.getPrefix(), "class");
+	    assertEquals("Variable is considered a class", "class", type
+		    .getPrefix());
 	else
-	    assertEquals("Variable is a struct", type.getPrefix(), "struct");
+	    assertEquals("Variable is a struct", "struct", type.getPrefix());
     }
 
 }
