882c8ebdcc825139e58e50982810a0f00f1a229b
Stan Cox <scox@redhat.com>
2008-01-16 21:39:41 -0500

    Change line marker to ->
    * ListCommand.java (interpret): Change line marker to ->
    * TestListCommand.java (testListPC): Ditto.

--- frysk-core/frysk/hpd/ListCommand.java
+++ frysk-core/frysk/hpd/ListCommand.java
@@ -1,6 +1,6 @@
 // This file is part of the program FRYSK.
 //
-// Copyright 2005, 2006, 2007 Red Hat Inc.
+// Copyright 2005, 2006, 2007, 2008 Red Hat Inc.
 //
 // FRYSK is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
@@ -187,9 +187,9 @@ class ListCommand extends ParameterizedCommand {
                     else if (lr.getLineNumber() == endLine)
                         break;
                     if (display && lr.getLineNumber() == exec_line)
-                        flag = "*";
+                        flag = "->";
 		    else
-			flag = " ";
+			flag = "  ";
 
                     if (display) {
 			int lineNumber = lr.getLineNumber();
