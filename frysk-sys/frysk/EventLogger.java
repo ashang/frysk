9217bc52ea47dd010f68d87426a70b491cbf4689
Andrew Cagney <cagney@redhat.com>
2008-02-11 13:52:51 -0500

    Rename -trace to -debug.
    frysk-sys/frysk/ChangeLog
    2008-02-11  Andrew Cagney  <cagney@redhat.com>
    
    	* EventLogger.java: Rename LogOption to "debug".

--- frysk-sys/frysk/EventLogger.java
+++ frysk-sys/frysk/EventLogger.java
@@ -1,6 +1,6 @@
 // This file is part of the program FRYSK.
 //
-// Copyright 2005, Red Hat Inc.
+// Copyright 2005, 2006, 2007, 2008, Red Hat Inc.
 //
 // FRYSK is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
@@ -232,6 +232,6 @@ public class EventLogger
 	    verbose = true;
 	  }
 	});
-      parser.add(new LogOption("trace"));
+      parser.add(new LogOption("debug"));
     }
 }
