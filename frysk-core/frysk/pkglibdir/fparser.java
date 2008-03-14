bd3b09d90dfd03c064314fb8a86e9a64a77ce0eb
Andrew Cagney <cagney@redhat.com>
2008-03-14 15:49:54 -0400

    Fix html man-page nits.
    frysk-common/ChangeLog
    2008-03-14  Andrew Cagney  <cagney@redhat.com>
    
    	* manpages.sh: New.
    	* frysk.xml: New.
    
    frysk-core/frysk/bindir/ChangeLog
    2008-03-14  Andrew Cagney  <cagney@redhat.com>
    
    	* frysk.xml: New.
    	* fparser.xml, fparser.java: Move to frysk.pkglibdir.
    	* fauxv.xml: Cite frysk(7), simplify url.
    	* fcatch.xml: Ditto.
    	* fcore.xml: Ditto.
    	* fdebuginfo.xml: Ditto.
    	* fdebugrpm.xml: Ditto.
    	* ferror.xml: Ditto.
    	* fexe.xml: Ditto.
    	* fhpd.xml: Ditto.
    	* fmaps.xml: Ditto.
    	* fparser.xml: Ditto.
    	* fstack.xml: Ditto.
    	* fstep.xml: Ditto.
    	* ftrace.xml: Ditto.
    
    frysk-core/frysk/pkglibdir/ChangeLog
    2008-03-14  Andrew Cagney  <cagney@redhat.com>
    
    	* fparser.xml, fparser.java: Move to here from frysk.bindir.
    	* funit-threads.xml: Cite frysk(7); update url.
    	* funit-slave.xml: Ditto.
    	* funit-exit.xml: Ditto.
    	* funit-exec.xml: Ditto.
    
    frysk-gui/frysk/bindir/ChangeLog
    2008-03-14  Andrew Cagney  <cagney@redhat.com>
    
    	* frysk.xml: Cite frysk(7).
    
    frysk-sys/frysk/pkglibdir/ChangeLog
    2008-03-14  Andrew Cagney  <cagney@redhat.com>
    
    	* fsystest.xml: Cite frysk(7), update url.
    	* funit-procmask.xml: Ditto.
    
    frysk-top/ChangeLog
    2008-03-14  Andrew Cagney  <cagney@redhat.com>
    
    	* Makefile.am (frysk-common/frysk.7): New target.
    	(manpages): Move code to frysk-common/manpages.sh.

--- frysk-core/frysk/pkglibdir/fparser.java
+++ frysk-core/frysk/pkglibdir/fparser.java
@@ -37,7 +37,7 @@
 // version and license this file solely under the GPL without
 // exception.
 
-package frysk.bindir;
+package frysk.pkglibdir;
 
 import frysk.dom.DOMFrysk;
 import frysk.dom.DOMImage;
