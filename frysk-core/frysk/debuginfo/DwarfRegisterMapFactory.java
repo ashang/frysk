6daa50892246080c78e41bf8fa0ef6f717ea7286
Thiago Jung Bauermann <bauerman@br.ibm.com>
2008-04-15 12:10:18 -0300

    Add DWARF register mapping for PowerPC64.
    frysk-core/frysk/debuginfo/ChangeLog
    2008-04-15  Thiago Jung Bauermann  <bauerman@br.ibm.com>
    
    	* DwarfRegisterMapFactory.java (PPC64BE): New.
    	(isaToMap): Add PPC64BE.
    
    frysk-sys/lib/dwfl/ChangeLog
    2008-04-15  Thiago Jung Bauermann  <bauerman@br.ibm.com>
    
    	* DwarfRegistersPPC64.mkenum: New.

--- frysk-core/frysk/debuginfo/DwarfRegisterMapFactory.java
+++ frysk-core/frysk/debuginfo/DwarfRegisterMapFactory.java
@@ -41,11 +41,13 @@ package frysk.debuginfo;
 
 import lib.dwfl.DwarfRegistersX86;
 import lib.dwfl.DwarfRegistersX8664;
+import lib.dwfl.DwarfRegistersPPC64;
 import frysk.isa.ISA;
 import frysk.isa.ISAMap;
 import frysk.isa.registers.IA32Registers;
 import frysk.isa.registers.RegisterMap;
 import frysk.isa.registers.X8664Registers;
+import frysk.isa.registers.PPC64Registers;
 
 public class DwarfRegisterMapFactory {
 
@@ -82,10 +84,84 @@ public class DwarfRegisterMapFactory {
 	.add(X8664Registers.RIP, DwarfRegistersX8664.RIP)
 	;
 
+    private static final RegisterMap PPC64BE
+	= new RegisterMap("PowerPC64 DWARF")
+	.add(PPC64Registers.GPR0, DwarfRegistersPPC64.GPR0)
+	.add(PPC64Registers.GPR1, DwarfRegistersPPC64.GPR1)
+	.add(PPC64Registers.GPR2, DwarfRegistersPPC64.GPR2)
+	.add(PPC64Registers.GPR3, DwarfRegistersPPC64.GPR3)
+	.add(PPC64Registers.GPR4, DwarfRegistersPPC64.GPR4)
+	.add(PPC64Registers.GPR5, DwarfRegistersPPC64.GPR5)
+	.add(PPC64Registers.GPR6, DwarfRegistersPPC64.GPR6)
+	.add(PPC64Registers.GPR7, DwarfRegistersPPC64.GPR7)
+	.add(PPC64Registers.GPR8, DwarfRegistersPPC64.GPR8)
+	.add(PPC64Registers.GPR9, DwarfRegistersPPC64.GPR9)
+	.add(PPC64Registers.GPR10, DwarfRegistersPPC64.GPR10)
+	.add(PPC64Registers.GPR11, DwarfRegistersPPC64.GPR11)
+	.add(PPC64Registers.GPR12, DwarfRegistersPPC64.GPR12)
+	.add(PPC64Registers.GPR13, DwarfRegistersPPC64.GPR13)
+	.add(PPC64Registers.GPR14, DwarfRegistersPPC64.GPR14)
+	.add(PPC64Registers.GPR15, DwarfRegistersPPC64.GPR15)
+	.add(PPC64Registers.GPR16, DwarfRegistersPPC64.GPR16)
+	.add(PPC64Registers.GPR17, DwarfRegistersPPC64.GPR17)
+	.add(PPC64Registers.GPR18, DwarfRegistersPPC64.GPR18)
+	.add(PPC64Registers.GPR19, DwarfRegistersPPC64.GPR19)
+	.add(PPC64Registers.GPR20, DwarfRegistersPPC64.GPR20)
+	.add(PPC64Registers.GPR21, DwarfRegistersPPC64.GPR21)
+	.add(PPC64Registers.GPR22, DwarfRegistersPPC64.GPR22)
+	.add(PPC64Registers.GPR23, DwarfRegistersPPC64.GPR23)
+	.add(PPC64Registers.GPR24, DwarfRegistersPPC64.GPR24)
+	.add(PPC64Registers.GPR25, DwarfRegistersPPC64.GPR25)
+	.add(PPC64Registers.GPR26, DwarfRegistersPPC64.GPR26)
+	.add(PPC64Registers.GPR27, DwarfRegistersPPC64.GPR27)
+	.add(PPC64Registers.GPR28, DwarfRegistersPPC64.GPR28)
+	.add(PPC64Registers.GPR29, DwarfRegistersPPC64.GPR29)
+	.add(PPC64Registers.GPR30, DwarfRegistersPPC64.GPR30)
+	.add(PPC64Registers.GPR31, DwarfRegistersPPC64.GPR31)
+	.add(PPC64Registers.FPR0, DwarfRegistersPPC64.FPR0)
+	.add(PPC64Registers.FPR1, DwarfRegistersPPC64.FPR1)
+	.add(PPC64Registers.FPR2, DwarfRegistersPPC64.FPR2)
+	.add(PPC64Registers.FPR3, DwarfRegistersPPC64.FPR3)
+	.add(PPC64Registers.FPR4, DwarfRegistersPPC64.FPR4)
+	.add(PPC64Registers.FPR5, DwarfRegistersPPC64.FPR5)
+	.add(PPC64Registers.FPR6, DwarfRegistersPPC64.FPR6)
+	.add(PPC64Registers.FPR7, DwarfRegistersPPC64.FPR7)
+	.add(PPC64Registers.FPR8, DwarfRegistersPPC64.FPR8)
+	.add(PPC64Registers.FPR9, DwarfRegistersPPC64.FPR9)
+	.add(PPC64Registers.FPR10, DwarfRegistersPPC64.FPR10)
+	.add(PPC64Registers.FPR11, DwarfRegistersPPC64.FPR11)
+	.add(PPC64Registers.FPR12, DwarfRegistersPPC64.FPR12)
+	.add(PPC64Registers.FPR13, DwarfRegistersPPC64.FPR13)
+	.add(PPC64Registers.FPR14, DwarfRegistersPPC64.FPR14)
+	.add(PPC64Registers.FPR15, DwarfRegistersPPC64.FPR15)
+	.add(PPC64Registers.FPR16, DwarfRegistersPPC64.FPR16)
+	.add(PPC64Registers.FPR17, DwarfRegistersPPC64.FPR17)
+	.add(PPC64Registers.FPR18, DwarfRegistersPPC64.FPR18)
+	.add(PPC64Registers.FPR19, DwarfRegistersPPC64.FPR19)
+	.add(PPC64Registers.FPR20, DwarfRegistersPPC64.FPR20)
+	.add(PPC64Registers.FPR21, DwarfRegistersPPC64.FPR21)
+	.add(PPC64Registers.FPR22, DwarfRegistersPPC64.FPR22)
+	.add(PPC64Registers.FPR23, DwarfRegistersPPC64.FPR23)
+	.add(PPC64Registers.FPR24, DwarfRegistersPPC64.FPR24)
+	.add(PPC64Registers.FPR25, DwarfRegistersPPC64.FPR25)
+	.add(PPC64Registers.FPR26, DwarfRegistersPPC64.FPR26)
+	.add(PPC64Registers.FPR27, DwarfRegistersPPC64.FPR27)
+	.add(PPC64Registers.FPR28, DwarfRegistersPPC64.FPR28)
+	.add(PPC64Registers.FPR29, DwarfRegistersPPC64.FPR29)
+	.add(PPC64Registers.FPR30, DwarfRegistersPPC64.FPR30)
+	.add(PPC64Registers.FPR31, DwarfRegistersPPC64.FPR31)
+	.add(PPC64Registers.CCR, DwarfRegistersPPC64.CCR)
+	.add(PPC64Registers.FPSCR, DwarfRegistersPPC64.FPSCR)
+	.add(PPC64Registers.XER, DwarfRegistersPPC64.XER)
+	.add(PPC64Registers.LR, DwarfRegistersPPC64.LR)
+	.add(PPC64Registers.CTR, DwarfRegistersPPC64.CTR)
+	;
+
     private static final ISAMap isaToMap
 	= new ISAMap("DwarfRegisterMapFactory")
 	.put(ISA.IA32, IA32)
 	.put(ISA.X8664, X8664)
+	.put(ISA.PPC64BE, PPC64BE)
 	;
 
     public static RegisterMap getRegisterMap(ISA isa) {
