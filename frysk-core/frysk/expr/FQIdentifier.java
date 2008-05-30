0d47203d34e3a73153c0f9f8058b04a65758cf92
Petr Machata <pmachata@redhat.com>
2008-05-30 19:12:33 +0200

    FQIdentifier has soname matching capabilities

--- frysk-core/frysk/expr/FQIdentifier.java
+++ frysk-core/frysk/expr/FQIdentifier.java
@@ -39,6 +39,12 @@
 
 package frysk.expr;
 
+// Both necessary for soname expansion.  In principle this doesn't
+// belong here, but it doesn't seem to be worth it to create separate
+// class just for soname expansion capabilities.  Right now at least.
+import frysk.dwfl.ObjectFile;
+import frysk.proc.Task;
+
 public class FQIdentifier {
 
     final public String soname;
@@ -48,6 +54,13 @@ public class FQIdentifier {
     final public String symbol;
     final public String version;
     final public boolean wantPlt;
+
+    final private int metasoname;
+    final private static int soname_null = -1;
+    final private static int soname_name = 0;
+    final private static int soname_MAIN = 2;
+    final private static int soname_INTERP = 3;
+    final private boolean sonameIsPath;
 
     public FQIdentifier(FQIdentToken tok) {
 	this.soname = tok.dso;
@@ -61,5 +74,69 @@ public class FQIdentifier {
 	    this.line = new Long(Long.parseLong(tok.line, 10));
 	else
 	    this.line = null;
+
+	if (soname == null) {
+	    this.sonameIsPath = false;
+	    this.metasoname = soname_null;
+	} else {
+	    this.sonameIsPath = soname.indexOf('/') != -1;
+	    if (soname.equals("MAIN"))
+		this.metasoname = soname_MAIN;
+	    else if (soname.equals("INTERP"))
+		this.metasoname = soname_INTERP;
+	    else
+		this.metasoname = soname_name;
+	}
+    }
+
+    public String toString() {
+	StringBuffer buf = new StringBuffer();
+	if (soname != null)
+	    buf.append('#').append(soname).append('#');
+	if (file != null)
+	    buf.append(file).append('#');
+	if (line != null)
+	    buf.append(line.longValue()).append('#');
+	if (proc != null)
+	    buf.append(proc).append('#');
+	if (wantPlt)
+	    buf.append("plt:");
+	buf.append(symbol);
+	if (version != null)
+	    buf.append('@').append(version);
+	return buf.toString();
+    }
+
+    /**
+     * Translate MAIN and INTERP meta-sonames to main binary and
+     * dynamic linker of given task.
+     */
+    public String expandSoname(Task task) {
+	if (metasoname == soname_null)
+	    return null;
+	else if (metasoname == soname_name)
+	    return soname;
+	else {
+	    // Don't cache so that the identifier is usable for
+	    // multiple tasks.
+	    String path = task.getProc().getExeFile().getSysRootedPath();
+	    ObjectFile objf = ObjectFile.buildFromFile(path);
+	    if (metasoname == soname_MAIN)
+		return objf.getSoname();
+	    else
+		return ObjectFile.buildFromFile(objf.getInterp()).getSoname();
+	}
+    }
+
+    /**
+     * Check if given ObjectFile WHAT matches this soname identifier.
+     */
+    public boolean sonameMatches(Task task, ObjectFile what) {
+	if (metasoname == soname_null)
+	    return true;
+	else if (sonameIsPath)
+	    return soname.equals(what.getFilename().getPath());
+	else
+	    return this.expandSoname(task).equals(what.getSoname());
     }
 }
